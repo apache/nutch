d698f0ef95f9d3dbf56d16b1f12b3b0004e547af
Douglass Cutting <cutting@apache.org>
2005-08-26 19:07:35 +0000

    Add scripts to manage mapred/ndfs hosts.
    git-svn-id: https://svn.eu.apache.org/repos/asf/lucene/nutch/branches/mapred@240308 13f79535-47bb-0310-9956-ffa450edef68

---
+++ bin/slaves.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# 
+# Run a shell command on all slave hosts.
+#
+# Environment Variables
+#
+#   NUTCH_SLAVES    File naming remote hosts.
+##
+
+usage="Usage: slaves.sh command..."
+
+# if no args specified, show usage
+if [ $# -le 0 ]; then
+  echo $usage
+  exit 1
+fi
+
+if [ "$NUTCH_SLAVES" = "" ]; then
+  echo Set NUTCH_SLAVES to file listing slave host names.
+  exit 1
+fi
+
+for slave in `cat $NUTCH_SLAVES`; do
+ echo -n $slave:\ 
+ ssh -o ConnectTimeout=1 $slave "$@"
+done
