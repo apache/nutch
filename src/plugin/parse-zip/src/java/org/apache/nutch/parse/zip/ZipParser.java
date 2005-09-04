561ca324bd655e791573506923b02b24f77cafdd
Jerome Charron <jerome@apache.org>
2005-09-04 21:26:45 +0000

    ASF License added
    git-svn-id: https://svn.eu.apache.org/repos/asf/lucene/nutch/trunk@278630 13f79535-47bb-0310-9956-ffa450edef68

--- src/plugin/parse-zip/src/java/org/apache/nutch/parse/zip/ZipParser.java
+++ src/plugin/parse-zip/src/java/org/apache/nutch/parse/zip/ZipParser.java
@@ -1,7 +1,17 @@
-/*
- * ZipParser.java
+/**
+ * Copyright 2005 The Apache Software Foundation
  *
- * Nutch parse plugin for zip files - Content Type : application/zip
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.apache.nutch.parse.zip;
@@ -24,9 +34,9 @@ import org.apache.nutch.protocol.Content;
 import org.apache.nutch.util.LogFormatter;
 
 /**
- *
+ * ZipParser class based on MSPowerPointParser class by Stephan Strittmatter.
+ * Nutch parse plugin for zip files - Content Type : application/zip
  * @author Rohit Kulkarni & Ashish Vaidya
- * ZipParser class based on MSPowerPointParser class by Stephan Strittmatter
  */
 public class ZipParser implements Parser{
     
