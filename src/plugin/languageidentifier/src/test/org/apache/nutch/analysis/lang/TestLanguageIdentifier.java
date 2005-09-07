f255eccde285704b669383540b15cefd5b6c3181
Piotr Kosiorowski <pkosiorowski@apache.org>
2005-09-07 18:05:03 +0000

    Fixed failing JUnit test on Windows
    git-svn-id: https://svn.apache.org/repos/asf/lucene/nutch/trunk@279391 13f79535-47bb-0310-9956-ffa450edef68

--- src/plugin/languageidentifier/src/test/org/apache/nutch/analysis/lang/TestLanguageIdentifier.java
+++ src/plugin/languageidentifier/src/test/org/apache/nutch/analysis/lang/TestLanguageIdentifier.java
@@ -213,7 +213,7 @@ public class TestLanguageIdentifier extends TestCase {
                 if (!tokens[0].equals("")) {
                     long start = System.currentTimeMillis();
                     // Identify the whole file
-                    String lang = idfr.identify(this.getClass().getResourceAsStream(tokens[0]));
+                    String lang = idfr.identify(this.getClass().getResourceAsStream(tokens[0]), "UTF-8");
                     total += System.currentTimeMillis() - start;
                     assertEquals(tokens[1], lang);
                     // Then, each line of the file...
