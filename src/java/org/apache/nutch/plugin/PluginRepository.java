ddcdad641a90c01eab6471efa623efe36d6c7a51
Andrzej Bialecki <ab@apache.org>
2006-02-02 10:58:16 +0000

    Add functionality to run individual classes in plugins.
    Add a user-friendly shortcut to bin/nutch (the "plugin" command).
    
    Fix missing setConf() in Http plugins.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/lucene/nutch/trunk@374348 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/nutch/plugin/PluginRepository.java
+++ src/java/org/apache/nutch/plugin/PluginRepository.java
@@ -19,6 +19,7 @@ package org.apache.nutch.plugin;
 // JDK imports
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -376,5 +377,47 @@ public class PluginRepository {
         map.put(plugin.getPluginId(), plugin);
       }
       return map;
+    }
+
+    /**
+     * Loads all necessary dependencies for a selected plugin, and then
+     * runs one of the classes' main() method.
+     * @param args plugin ID (needs to be activated in the configuration), and
+     * the class name. The rest of arguments is passed to the main method of the
+     * selected class.
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+      if (args.length < 2) {
+        System.err.println("Usage: PluginRepository pluginId className [arg1 arg2 ...]");
+        return;
+      }
+      NutchConf conf = new NutchConf();
+      PluginRepository repo = new PluginRepository(conf);
+      // args[0] - plugin ID
+      PluginDescriptor d = repo.getPluginDescriptor(args[0]);
+      if (d == null) {
+        System.err.println("Plugin '" + args[0] + "' not present or inactive.");
+        return;
+      }
+      ClassLoader cl = d.getClassLoader();
+      // args[1] - class name
+      Class clazz = null;
+      try {
+        clazz = Class.forName(args[1], true, cl);
+      } catch (Exception e) {
+        System.err.println("Could not load the class '" + args[1] + ": " + e.getMessage());
+        return;
+      }
+      Method m = null;
+      try {
+        m = clazz.getMethod("main", new Class[]{args.getClass()});
+      } catch (Exception e) {
+        System.err.println("Could not find the 'main(String[])' method in class " + args[1] + ": " + e.getMessage());
+        return;
+      }
+      String[] subargs = new String[args.length - 2];
+      System.arraycopy(args, 2, subargs, 0, subargs.length);
+      m.invoke(null, new Object[]{subargs});
     }
 }
