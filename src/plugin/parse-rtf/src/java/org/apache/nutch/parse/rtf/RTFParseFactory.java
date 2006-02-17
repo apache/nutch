d4fb676252ac0b8b86a9b510994977956cf3fa98
Jerome Charron <jerome@apache.org>
2006-02-17 23:22:55 +0000

    Adapt parse-rtf to nutch APIs changes (metadata, parse, protocol, ...)
    git-svn-id: https://svn.eu.apache.org/repos/asf/lucene/nutch/trunk@378653 13f79535-47bb-0310-9956-ffa450edef68

--- src/plugin/parse-rtf/src/java/org/apache/nutch/parse/rtf/RTFParseFactory.java
+++ src/plugin/parse-rtf/src/java/org/apache/nutch/parse/rtf/RTFParseFactory.java
@@ -13,38 +13,42 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.nutch.parse.rtf;
 
-import org.apache.nutch.parse.*;
-import org.apache.nutch.protocol.Content;
-<<<<<<< .mine
-<<<<<<< .mine
-import org.apache.nutch.util.MetadataNames;
-
-=======
-import org.apache.nutch.util.NutchConf;
-=======
-import org.apache.hadoop.conf.Configuration;
->>>>>>> .r374853
->>>>>>> .r373941
+// JDK imports
 import java.io.ByteArrayInputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.util.Properties;
 
+// Hadoop imports
+import org.apache.hadoop.conf.Configuration;
+
+// Nutch imports
+import org.apache.nutch.metadata.DublinCore;
+import org.apache.nutch.metadata.Metadata;
+import org.apache.nutch.parse.OutlinkExtractor;
+import org.apache.nutch.parse.Parse;
+import org.apache.nutch.parse.ParseData;
+import org.apache.nutch.parse.ParseImpl;
+import org.apache.nutch.parse.ParseStatus;
+import org.apache.nutch.parse.Parser;
+import org.apache.nutch.protocol.Content;
+
+// RTF Parser imports
+import com.etranslate.tm.processing.rtf.ParseException;
 import com.etranslate.tm.processing.rtf.RTFParser;
+
 
 /**
  * A parser for RTF documents
  * 
  * @author Andy Hedges
  */
-public class RTFParseFactory implements Parser, MetadataNames {
+public class RTFParseFactory implements Parser {
 
   private Configuration conf;
 
-  public Parse getParse(Content content) throws ParseException {
+  public Parse getParse(Content content) {
     byte[] raw = content.getContent();
     Reader reader = new InputStreamReader(new ByteArrayInputStream(raw));
     RTFParserDelegateImpl delegate = new RTFParserDelegateImpl();
@@ -55,28 +59,31 @@ public class RTFParseFactory implements Parser, MetadataNames {
 
     try {
       rtfParser.parse();
-    } catch (com.etranslate.tm.processing.rtf.ParseException e) {
-      throw new ParseException("Exception parsing RTF document", e);
+    } catch (ParseException e) {
+        return new ParseStatus(ParseStatus.FAILED,
+                               ParseStatus.FAILED_EXCEPTION,
+                               e.toString()).getEmptyParse(conf);
     }
 
-    Properties metadata = new Properties();
-    metadata.putAll(content.getMetadata());
-    metadata.putAll(delegate.getMetaData());
-    String title = metadata.getProperty(TITLE);
+    Metadata metadata = new Metadata();
+    metadata.setAll(delegate.getMetaData());
+    String title = metadata.get(DublinCore.TITLE);
 
     if (title != null) {
-        //(CM): Why remove the title metadata property here? Even 
-        //though it's stored in the ParseData, it still might be useful
-        //to have via this properties object?
-        //metadata.remove(title);
+      metadata.remove(DublinCore.TITLE);
     } else {
       title = "";
     }
 
     String text = delegate.getText();
 
-    return new ParseImpl(text, new ParseData(title, OutlinkExtractor
-        .getOutlinks(text, this.conf), metadata));
+    return new ParseImpl(text,
+                         new ParseData(ParseStatus.STATUS_SUCCESS,
+                                       title,
+                                       OutlinkExtractor
+        .                              getOutlinks(text, this.conf),
+                                       content.getMetadata(),
+                                       metadata));
   }
 
   public void setConf(Configuration conf) {
