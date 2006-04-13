1ededff9745a51cf8e42f192d2136ed5d1388d4d
Piotr Kosiorowski <pkosiorowski@apache.org>
2006-04-13 18:57:55 +0000

    NUTCH-237 - Carrot2 clustering plugin upgrade. (Dawid Weiss)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/nutch/trunk@393889 13f79535-47bb-0310-9956-ffa450edef68

--- src/plugin/clustering-carrot2/src/java/org/apache/nutch/clustering/carrot2/LocalNutchInputComponent.java
+++ src/plugin/clustering-carrot2/src/java/org/apache/nutch/clustering/carrot2/LocalNutchInputComponent.java
@@ -34,6 +34,7 @@ import com.dawidweiss.carrot.core.local.LocalInputComponentBase;
 import com.dawidweiss.carrot.core.local.ProcessingException;
 import com.dawidweiss.carrot.core.local.RequestContext;
 import com.dawidweiss.carrot.core.local.clustering.*;
+import com.dawidweiss.carrot.util.common.StringUtils;
 
 /**
  * A local input component that ignores the query passed from the
@@ -58,7 +59,19 @@ public class LocalNutchInputComponent extends LocalInputComponentBase {
   /** This component's capabilities */
   private final static Set COMPONENT_CAPABILITIES 
     = new HashSet(Arrays.asList(new Object [] { RawDocumentsProducer.class }));
-    
+
+  /**
+   * Default language code for hits that don't have their own.
+   */
+  private String defaultLanguage;
+
+  /**
+   * Creates an input component with the given default language code.
+   */
+  public LocalNutchInputComponent(String defaultLanguage) {
+    this.defaultLanguage = defaultLanguage;
+  }
+
   /*
    * @see com.dawidweiss.carrot.core.local.LocalInputComponent#setQuery(java.lang.String)
    */
@@ -66,15 +79,17 @@ public class LocalNutchInputComponent extends LocalInputComponentBase {
       // ignore the query; data will be provided from the request context.
   }
 
-  /** A callback hook that starts the processing. */
+  /**
+   * A callback hook that starts the processing.
+   */
   public void startProcessing(RequestContext context) throws ProcessingException {
     // let successor components know that the processing has started.
     super.startProcessing(context);
     
     // get the information about documents from the context.
-    Map params = context.getRequestParameters();
-    HitDetails [] details = (HitDetails[]) params.get(NUTCH_INPUT_HIT_DETAILS_ARRAY);
-    String [] summaries = (String[]) params.get(NUTCH_INPUT_SUMMARIES_ARRAY);
+    final Map params = context.getRequestParameters();
+    final HitDetails [] details = (HitDetails[]) params.get(NUTCH_INPUT_HIT_DETAILS_ARRAY);
+    final String [] summaries = (String[]) params.get(NUTCH_INPUT_SUMMARIES_ARRAY);
     
     if (details == null)
       throw new ProcessingException("Details array must not be null.");
@@ -85,11 +100,10 @@ public class LocalNutchInputComponent extends LocalInputComponentBase {
     if (summaries.length != details.length)
       throw new ProcessingException("Summaries and details must be of the same length.");
     
-    RawDocumentsConsumer consumer = (RawDocumentsConsumer) next;
-    
     // produce 'documents' for successor components.
+    final RawDocumentsConsumer consumer = (RawDocumentsConsumer) next;
     for (int i=0;i<summaries.length;i++) {
-      consumer.addDocument(new NutchDocument(i, details[i], htmlToText(summaries[i])));
+      consumer.addDocument(new NutchDocument(i, details[i], htmlToText(summaries[i]), defaultLanguage));
     }
   }
 
@@ -107,52 +121,14 @@ public class LocalNutchInputComponent extends LocalInputComponentBase {
     return SUCCESSOR_CAPABILITIES;
   }
 
-  // --- The methods below, plus dependency on the Nekohtml parser
-  // are only required because Nutch's summaries are in HTML by default.
-  // I guess it would be possible to get rid of the code below by
-  // adding patches/ methods to Nutch that return plain text summaries.
-  // 
-  // The temporary quick-and-dirty solution below has been provided by Doug, thanks. 
-
-  /**
-   * The text buffer for plain text. 
-   */
-  private StringBuffer textBuffer = new StringBuffer();
-    
-  /**
-   * A parser that will convert html to plain text.
-   */
-  private AbstractSAXParser parser;
-
-  /*
-   * Anonymous initialization of the parser. Since we declared
-   * the current solution to be quick and dirty, it doesn't have
-   * to be in the constructor :)
-   */
-  {
-    try {
-      parser = new AbstractSAXParser(new HTMLConfiguration()){};
-      parser.setContentHandler(new DefaultHandler() {
-          public void characters(char[] chars, int start, int length)
-            throws SAXException {
-            textBuffer.append(chars, start, length);
-          }
-        });
-    } catch (Exception e) {
-      throw new RuntimeException(e.toString(), e);
-    }
-  }
-
   /**
    * Converts a html chunk to plain text.
+   * 
+   * This method is only required because Nutch's summaries are in HTML.
+   * I guess it would be possible to get rid of the code below by
+   * adding patches/ methods to Nutch that return plain text summaries. 
    */
   private final String htmlToText(String html) {
-    textBuffer.setLength(0);
-    try {
-      parser.parse(new InputSource(new StringReader(html)));
-    } catch (Exception e) {                     // shouldn't happen
-      throw new RuntimeException(e.toString(), e);
-    }
-    return textBuffer.toString();
+    return StringUtils.removeMarkup(html);
   }
 }
