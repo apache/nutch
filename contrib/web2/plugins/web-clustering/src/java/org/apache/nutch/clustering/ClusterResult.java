7833aceb4e878e498631441259765d60df4d917d
Sami Siren <siren@apache.org>
2006-05-28 18:18:13 +0000

    clustering web-ui-plugin initial version
    git-svn-id: https://svn.apache.org/repos/asf/lucene/nutch/trunk@409972 13f79535-47bb-0310-9956-ffa450edef68

---
+++ contrib/web2/plugins/web-clustering/src/java/org/apache/nutch/clustering/ClusterResult.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2006 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nutch.clustering;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.nutch.webapp.common.SearchResultBean;
+
+/** Wrapper for HitsCluster
+ * 
+ */
+public class ClusterResult {
+
+  private int numDocs;
+  private String labelString="";
+  HitsCluster cluster;
+  
+  public ClusterResult(HitsCluster cluster, int numLabels, int numDocs){
+    this.numDocs=numDocs;
+    this.cluster=cluster;
+    int maxSize=Math.min(numLabels, cluster.getDescriptionLabels().length);
+    for(int i=0;i<maxSize;i++){
+      if(i>0) {
+        labelString+=", ";
+      }
+      labelString+=cluster.getDescriptionLabels()[i];
+    }
+  }
+  
+  public String getLabel(){
+    return labelString;
+  }  
+  
+  /** return document samples of cluster
+   * 
+   * @return
+   */
+  public List getDocs(){
+    int maxNumDocs=Math.min(numDocs,cluster.getHits().length);
+    List docs=new ArrayList(maxNumDocs);
+    for(int i=0;i<maxNumDocs;i++){
+      docs.add(new SearchResultBean(null, null, cluster.getHits()[i],null));
+    }
+    return docs;
+  }
+
+}
---
+++ contrib/web2/plugins/web-clustering/src/java/org/apache/nutch/clustering/Clusters.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2006 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nutch.clustering;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Wrapper object to clustering results
+ */
+public class Clusters {
+  
+  private List clusters;
+
+  public Clusters(final HitsCluster [] clustersArray, final int numClusters, final int numDocs, final int numLabels){
+    clusters=new ArrayList();
+    for(int i=0;i<numClusters;i++){
+      clusters.add(new ClusterResult(clustersArray[i], numDocs, numLabels));
+    }
+  }
+  
+  public List getClusters(){
+    return clusters;
+  }
+  
+  public boolean getHasClusters(){
+    return clusters.size()>0;
+  }
+
+
+}
