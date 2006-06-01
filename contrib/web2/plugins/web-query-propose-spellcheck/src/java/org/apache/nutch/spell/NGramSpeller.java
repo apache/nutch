5ad398475ad0763b8318d7c6b931e713e2fc3c68
Sami Siren <siren@apache.org>
2006-06-01 16:56:30 +0000

    fixed license headers
    git-svn-id: https://svn.eu.apache.org/repos/asf/lucene/nutch/trunk@410887 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/web2/plugins/web-query-propose-spellcheck/src/java/org/apache/nutch/spell/NGramSpeller.java
+++ contrib/web2/plugins/web-query-propose-spellcheck/src/java/org/apache/nutch/spell/NGramSpeller.java
@@ -1,5 +1,5 @@
-/**
- * Copyright 2005 The Apache Software Foundation
+/*
+ * Copyright 2006 The Apache Software Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.nutch.spell;
 
 import org.apache.lucene.analysis.*;
