4d53c82688145800eda4a60adf43ae1e0200514e
Sami Siren <siren@apache.org>
2006-06-02 18:49:48 +0000

    removed unused imports
    git-svn-id: https://svn.eu.apache.org/repos/asf/lucene/nutch/trunk@411240 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/nutch/plugin/PluginManifestParser.java
+++ src/java/org/apache/nutch/plugin/PluginManifestParser.java
@@ -33,11 +33,8 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.apache.hadoop.conf.Configuration;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.EntityResolver;
-import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 /**
