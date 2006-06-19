53d32f722db195b06d82a6398c48bbc15bcff2d5
Sami Siren <siren@apache.org>
2006-06-19 17:30:13 +0000

    fixed caching to store entries to disk as promised
    git-svn-id: https://svn.apache.org/repos/asf/lucene/nutch/trunk@415379 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/web2/plugins/web-caching-oscache/src/java/org/apache/nutch/webapp/CacheManager.java
+++ contrib/web2/plugins/web-caching-oscache/src/java/org/apache/nutch/webapp/CacheManager.java
@@ -15,21 +15,58 @@
  */
 package org.apache.nutch.webapp;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Properties;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.nutch.webapp.common.Search;
+import org.apache.nutch.webapp.common.ServiceLocator;
 
 import com.opensymphony.oscache.base.Cache;
 import com.opensymphony.oscache.base.CacheEntry;
 import com.opensymphony.oscache.base.EntryRefreshPolicy;
 import com.opensymphony.oscache.base.NeedsRefreshException;
+import com.opensymphony.oscache.general.GeneralCacheAdministrator;
 
 /**
- * CacheManager for 
+ * This class is responsible for configuring the used cache and
+ * delivering cached Search objects.
+ * 
+ * Configuration parameters can be overrided with default nutch
+ * configuration mechanism.
+ * 
+ * Search Objects are compressed for smaller space requirements.
  */
 public class CacheManager {
-
-  static final String CACHE_KEY="cache";
   
+  public static class ByteBufferWrapper implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    byte[] contents;
+    
+    public ByteBufferWrapper(final byte[] contents){
+      this.contents=contents;
+    }
+    
+    public byte[] getContents(){
+      return contents;
+    }
+    
+  }
+
+  static final Log LOG=LogFactory.getLog(CacheManager.class);
+  
+  static final String CACHE_KEY=CacheManager.class.getName();
+
   class NutchRefreshPolicy implements EntryRefreshPolicy {
 
     private static final long serialVersionUID = 1L;
@@ -41,17 +78,39 @@ public class CacheManager {
   
   EntryRefreshPolicy policy=new NutchRefreshPolicy();
 
+
   Cache cache;
+  GeneralCacheAdministrator cacheadmin;
   
-  protected CacheManager(){
-    cache=new Cache(true,true,false,true,"com.opensymphony.oscache.base.algorithm.UnlimitedCache",Integer.MAX_VALUE);
+  protected CacheManager(Configuration conf){
+    
+    Properties p=new Properties();
+    
+    //use memory for caching
+    boolean cacheMemory=conf.getBoolean("cache.memory", false);
+    p.setProperty("cache.memory", Boolean.toString(cacheMemory));
+    
+    //the persistence class used
+    String cachePersistenceClass=conf.get("cache.persistence.class","org.apache.nutch.cache.CustomDiskPersistenceListener");
+    p.setProperty("cache.persistence.class", cachePersistenceClass);
+
+    //where to store cache files (if file cache used)
+    String cachePath=conf.get("cache.path", ".");
+    p.setProperty("cache.path", cachePath);
+
+    //cacacity of cache (how many entries)
+    int cacheCapacity=conf.getInt("cache.capacity", 1000);
+    p.setProperty("cache.capacity", Integer.toString(cacheCapacity));
+    
+    cacheadmin=new GeneralCacheAdministrator(p);
+    cache=cacheadmin.getCache();
   }
   
-  public static CacheManager getInstance(Configuration conf){
+  public synchronized static CacheManager getInstance(Configuration conf){
     CacheManager cache=(CacheManager)conf.getObject(CACHE_KEY);
     
     if(cache==null) {
-      cache = new CacheManager();
+      cache = new CacheManager(conf);
       
       conf.setObject(CACHE_KEY, cache);
     }
@@ -64,8 +123,33 @@ public class CacheManager {
    * @return
    * @throws NeedsRefreshException
    */
-  public Search getSearch(String id) throws NeedsRefreshException  {
-    return (Search) cache.getFromCache(id);
+  public Search getSearch(String id, ServiceLocator locator) throws NeedsRefreshException  {
+    Search search=null;
+    
+    ByteBufferWrapper w=(ByteBufferWrapper)cache.getFromCache(id);
+    if(w!=null){
+      
+
+      try {
+        long time=System.currentTimeMillis();
+        ByteArrayInputStream is=new ByteArrayInputStream(w.getContents());
+        GZIPInputStream gs = new GZIPInputStream(is);
+        DataInputStream dis = new DataInputStream(gs);
+
+        search = new Search(locator);
+        search.readFields(dis);
+        long delta=System.currentTimeMillis()-time;
+        
+        if(LOG.isDebugEnabled()){
+          LOG.debug("Decompressing cache entry took: " + delta + "ms.");
+        }
+
+        search.init();
+      } catch (IOException e) {
+        LOG.info("Could not get cached object: " + e);
+      }
+    } 
+    return search;
   }
 
   /**
@@ -75,7 +159,28 @@ public class CacheManager {
    * @param search the search to cache
    */
   public void putSearch(String id, Search search){
-    cache.putInCache(id,search,policy);
+    try {
+      long time=System.currentTimeMillis();
+      ByteArrayOutputStream bos=new ByteArrayOutputStream();
+      GZIPOutputStream gzos=new GZIPOutputStream(bos);
+      DataOutputStream oos=new DataOutputStream(gzos);
+      search.write(oos);
+      oos.flush();
+      oos.close();
+      gzos.close();
+      long delta=System.currentTimeMillis()-time;
+      ByteBufferWrapper wrap=new ByteBufferWrapper(bos.toByteArray());
+      if(LOG.isDebugEnabled()){
+        LOG.debug("Compressing cache entry took: " + delta + "ms.");
+        LOG.debug("size: " + wrap.getContents().length + " bytes");
+      }
+      cache.putInCache(id, wrap);
+    } catch (IOException e) {
+      LOG.info("cannot store object in cache: " + e);
+    }
   }
 
+  public void cancelUpdate(String key) {
+    cache.cancelUpdate(key);
+  }
 }
