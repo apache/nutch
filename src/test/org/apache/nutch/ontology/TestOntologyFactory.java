e156a5625ed02b9e593ac79dbcb3339d9fa31eef
Sami Siren <siren@apache.org>
2006-07-27 13:27:42 +0000

    add some unit test for different factories
    git-svn-id: https://svn.apache.org/repos/asf/lucene/nutch/trunk@426065 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/nutch/ontology/TestOntologyFactory.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2006 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nutch.ontology;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.nutch.plugin.PluginRuntimeException;
+import org.apache.nutch.util.NutchConfiguration;
+
+import junit.framework.TestCase;
+
+public class TestOntologyFactory extends TestCase {
+
+  private Configuration conf;
+  
+  protected void setUp() throws Exception {
+      conf = NutchConfiguration.create();
+      conf.set("plugin.includes", ".*");
+  }
+
+  public void testGetOntology() {
+    OntologyFactory factory=new OntologyFactory(conf);
+    
+    try {
+      Ontology ontology1=factory.getOntology();
+      Ontology ontology2=factory.getOntology();
+      assertNotNull(ontology1);
+      assertNotNull(ontology2);
+      //Current implementation creates new object instance in every call
+      //TODO: check if this is required  
+      assertNotSame(ontology1, ontology2);
+    } catch (PluginRuntimeException e) {
+      fail("should not trow:" + e);
+    }
+  }
+}
---
+++ src/test/org/apache/nutch/searcher/TestSummarizerFactory.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2006 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nutch.searcher;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.nutch.util.NutchConfiguration;
+
+import junit.framework.TestCase;
+
+public class TestSummarizerFactory extends TestCase {
+  
+  private Configuration conf;
+  
+  protected void setUp() throws Exception {
+      conf = NutchConfiguration.create();
+      conf.set("plugin.includes", ".*");
+  }
+
+  public void testGetSummarizer(){
+    SummarizerFactory factory=new SummarizerFactory(conf);
+    Summarizer summarizer1=factory.getSummarizer();
+    Summarizer summarizer2=factory.getSummarizer();
+    
+    assertNotNull(summarizer1);
+    assertEquals(summarizer1, summarizer2);
+  }
+}
