78db84d44067e20746d30e7f5d1d6a1ca347f188
Andrzej Bialecki <ab@apache.org>
2007-03-23 22:59:01 +0000

    Upgrade to Hadoop 0.12.2 release.
    Fix whitespace issues in platform name in bin/hadoop under Cygwin.
    
    Replace deprecated method call.
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/lucene/nutch/trunk@521933 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/nutch/indexer/TestDeleteDuplicates.java
+++ src/test/org/apache/nutch/indexer/TestDeleteDuplicates.java
@@ -57,7 +57,7 @@ public class TestDeleteDuplicates extends TestCase {
   private Path createIndex(String name, boolean hashDup, float inc, long time, boolean incFirst) throws Exception {
     Path idx = new Path(root, name);
     Path sub = new Path(idx, "part-0000");
-    Directory dir = FSDirectory.getDirectory(sub.toString(), true);
+    Directory dir = FSDirectory.getDirectory(sub.toString());
     IndexWriter writer = new IndexWriter(dir, new NutchDocumentAnalyzer(conf), true);
     Document doc = makeDoc(name,
         MD5Hash.digest("1").toString(),
