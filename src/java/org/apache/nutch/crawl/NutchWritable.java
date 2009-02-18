e30c6ca1ffc56680741714228e29dcb7bb2cb4c3
Sami Siren <siren@apache.org>
2009-02-18 09:05:02 +0000

    NUTCH-688 add missing headers, part 1 core
    git-svn-id: https://svn.apache.org/repos/asf/lucene/nutch/trunk@745441 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/nutch/crawl/NutchWritable.java
+++ src/java/org/apache/nutch/crawl/NutchWritable.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.nutch.crawl;
 
 import org.apache.hadoop.io.Writable;
@@ -8,7 +24,7 @@ public class NutchWritable extends GenericWritableConfigurable {
   private static Class<? extends Writable>[] CLASSES = null;
   
   static {
-    CLASSES = (Class<? extends Writable>[]) new Class[] {
+    CLASSES = new Class[] {
       org.apache.hadoop.io.NullWritable.class, 
       org.apache.hadoop.io.LongWritable.class,
       org.apache.hadoop.io.BytesWritable.class,
