6bee302e4b61012a75eb4815786782c89f27a1a1
Tacettin Guney <dogacan@apache.org>
2009-03-09 17:34:51 +0000

    NUTCH-684 - Dedup support for Solr
    git-svn-id: https://svn.apache.org/repos/asf/lucene/nutch/trunk@751774 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/nutch/indexer/solr/SolrConstants.java
+++ src/java/org/apache/nutch/indexer/solr/SolrConstants.java
@@ -22,5 +22,15 @@ public interface SolrConstants {
   public static final String SERVER_URL = SOLR_PREFIX + "server.url";
 
   public static final String COMMIT_SIZE = SOLR_PREFIX + "commit.size";
+  
+  public static final String ID_FIELD = "id";
+  
+  public static final String URL_FIELD = "url";
+  
+  public static final String BOOST_FIELD = "boost";
+  
+  public static final String TIMESTAMP_FIELD = "tstamp";
+  
+  public static final String DIGEST_FIELD = "digest";
 
 }
