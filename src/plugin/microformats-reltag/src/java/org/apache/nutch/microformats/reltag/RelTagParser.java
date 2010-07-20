514c02e9d3cfc13897e82b904c9075cab00e93ff
Julien Nioche <jnioche@apache.org>
2010-07-20 11:18:18 +0000

    ported microformat plugin
    git-svn-id: https://svn.apache.org/repos/asf/nutch/branches/nutchbase@965806 13f79535-47bb-0310-9956-ffa450edef68

--- src/plugin/microformats-reltag/src/java/org/apache/nutch/microformats/reltag/RelTagParser.java
+++ src/plugin/microformats-reltag/src/java/org/apache/nutch/microformats/reltag/RelTagParser.java
@@ -19,142 +19,141 @@ package org.apache.nutch.microformats.reltag;
 // JDK imports
 import java.net.URL;
 import java.net.URLDecoder;
+import java.nio.ByteBuffer;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
+
+import org.apache.avro.util.Utf8;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.nutch.parse.HTMLMetaTags;
+import org.apache.nutch.parse.HtmlParseFilter;
+import org.apache.nutch.parse.Parse;
+import org.apache.nutch.storage.WebPage;
+import org.apache.nutch.storage.WebPage.Field;
+import org.apache.nutch.util.StringUtil;
 import org.w3c.dom.DocumentFragment;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-// Commons Logging imports
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-// Nutch imports
-import org.apache.nutch.metadata.Metadata;
-import org.apache.nutch.parse.HTMLMetaTags;
-import org.apache.nutch.parse.Parse;
-import org.apache.nutch.parse.ParseResult;
-import org.apache.nutch.parse.HtmlParseFilter;
-import org.apache.nutch.protocol.Content;
-import org.apache.nutch.util.StringUtil;
-
-// Hadoop imports
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.io.Text;
-
-
 /**
  * Adds microformat rel-tags of document if found.
- *
+ * 
  * @see <a href="http://www.microformats.org/wiki/rel-tag">
  *      http://www.microformats.org/wiki/rel-tag</a>
  * @author J&eacute;r&ocirc;me Charron
  */
 public class RelTagParser implements HtmlParseFilter {
-  
-  public final static Log LOG = LogFactory.getLog(RelTagParser.class);
 
-  public final static String REL_TAG = "Rel-Tag";
-  
-  
-  private Configuration conf = null;
-  
-  
-  /**
-   * Scan the HTML document looking at possible rel-tags
-   */
-  public ParseResult filter(Content content, ParseResult parseResult,
-    HTMLMetaTags metaTags, DocumentFragment doc) {
-    
-    // get parse obj
-    Parse parse = parseResult.get(content.getUrl());
-    // Trying to find the document's rel-tags
-    Parser parser = new Parser(doc);
-    Set tags = parser.getRelTags();
-    Iterator iter = tags.iterator();
-    Metadata metadata = parse.getData().getParseMeta();
-    while (iter.hasNext()) {
-      metadata.add(REL_TAG, (String) iter.next());
-    }
-    return parseResult;
-  }
+	public final static Log LOG = LogFactory.getLog(RelTagParser.class);
 
-  private static class Parser {
+	public final static String REL_TAG = "Rel-Tag";
 
-    Set tags = null;
-    
-    Parser(Node node) {
-      tags = new TreeSet();
-      parse(node);
-    }
-  
-    Set getRelTags() {
-      return tags;
-    }
-    
-    void parse(Node node) {
+	private Configuration conf = null;
 
-      if (node.getNodeType() == Node.ELEMENT_NODE) {
-        // Look for <a> tag
-        if ("a".equalsIgnoreCase(node.getNodeName())) {
-          NamedNodeMap attrs = node.getAttributes();
-          Node hrefNode = attrs.getNamedItem("href");
-          // Checks that it contains a href attribute
-          if (hrefNode != null) {
-            Node relNode = attrs.getNamedItem("rel");
-            // Checks that it contains a rel attribute too
-            if (relNode != null) {
-              // Finaly checks that rel=tag
-              if ("tag".equalsIgnoreCase(relNode.getNodeValue())) {
-                String tag = parseTag(hrefNode.getNodeValue());
-                if (!StringUtil.isEmpty(tag)) {
-                  tags.add(tag);
-                }
-              }
-            }
-          }
-        }
-      }
-      
-      // Recurse
-      NodeList children = node.getChildNodes();
-      for (int i=0; children != null && i<children.getLength(); i++) {
-        parse(children.item(i));
-      }
-    }
-    
-    private final static String parseTag(String url) {
-      String tag = null;
-      try {
-        URL u = new URL(url);
-        String path = u.getPath();
-        tag = URLDecoder.decode(path.substring(path.lastIndexOf('/') + 1), "UTF-8");
-      } catch (Exception e) {
-        // Malformed tag...
-        tag = null;
-      }
-      return tag;
-    }
-    
-  }
+	private static class Parser {
 
+		Set<String> tags = null;
 
-  /* ----------------------------- *
-   * <implementation:Configurable> *
-   * ----------------------------- */
-  
-  public void setConf(Configuration conf) {
-    this.conf = conf;
-  }
+		Parser(Node node) {
+			tags = new TreeSet<String>();
+			parse(node);
+		}
 
-  public Configuration getConf() {
-    return this.conf;
-  }
-  
-  /* ------------------------------ *
-   * </implementation:Configurable> *
-   * ------------------------------ */
-  
+		Set<String> getRelTags() {
+			return tags;
+		}
+
+		void parse(Node node) {
+
+			if (node.getNodeType() == Node.ELEMENT_NODE) {
+				// Look for <a> tag
+				if ("a".equalsIgnoreCase(node.getNodeName())) {
+					NamedNodeMap attrs = node.getAttributes();
+					Node hrefNode = attrs.getNamedItem("href");
+					// Checks that it contains a href attribute
+					if (hrefNode != null) {
+						Node relNode = attrs.getNamedItem("rel");
+						// Checks that it contains a rel attribute too
+						if (relNode != null) {
+							// Finaly checks that rel=tag
+							if ("tag".equalsIgnoreCase(relNode.getNodeValue())) {
+								String tag = parseTag(hrefNode.getNodeValue());
+								if (!StringUtil.isEmpty(tag)) {
+									tags.add(tag);
+								}
+							}
+						}
+					}
+				}
+			}
+
+			// Recurse
+			NodeList children = node.getChildNodes();
+			for (int i = 0; children != null && i < children.getLength(); i++) {
+				parse(children.item(i));
+			}
+		}
+
+		private final static String parseTag(String url) {
+			String tag = null;
+			try {
+				URL u = new URL(url);
+				String path = u.getPath();
+				tag = URLDecoder.decode(
+						path.substring(path.lastIndexOf('/') + 1), "UTF-8");
+			} catch (Exception e) {
+				// Malformed tag...
+				tag = null;
+			}
+			return tag;
+		}
+
+	}
+
+	public void setConf(Configuration conf) {
+		this.conf = conf;
+	}
+
+	public Configuration getConf() {
+		return this.conf;
+	}
+
+	private static final Collection<WebPage.Field> FIELDS = new HashSet<WebPage.Field>();
+
+	static {
+		FIELDS.add(WebPage.Field.BASE_URL);
+		FIELDS.add(WebPage.Field.METADATA);
+	}
+	
+	@Override
+	public Collection<Field> getFields() {
+		return FIELDS;
+	}
+
+	@Override
+	/**
+	 * Scan the HTML document looking at possible rel-tags
+	 */
+	public Parse filter(String url, WebPage page, Parse parse,
+			HTMLMetaTags metaTags, DocumentFragment doc) {
+		// Trying to find the document's rel-tags
+		Parser parser = new Parser(doc);
+		Set<String> tags = parser.getRelTags();
+		// can't store multiple values in page metadata -> separate by tabs
+		StringBuffer sb = new StringBuffer();
+		Iterator<String> iter = tags.iterator();
+		while (iter.hasNext()) {
+			sb.append(iter.next());
+			sb.append("\t");
+		}
+		ByteBuffer bb = ByteBuffer.wrap(sb.toString().getBytes());
+		page.putToMetadata(new Utf8(REL_TAG), bb);
+		return parse;
+	}
 }
