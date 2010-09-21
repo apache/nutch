2bf54e7e6b84d0f39af9fc7648915c2eb2526db6
Chris Mattmann <mattmann@apache.org>
2010-09-21 01:53:51 +0000

    - fix for NUTCH-901 Make index-more plug-in configurable
    git-svn-id: https://svn.apache.org/repos/asf/nutch/trunk@999181 13f79535-47bb-0310-9956-ffa450edef68

--- src/plugin/index-more/src/java/org/apache/nutch/indexer/more/MoreIndexingFilter.java
+++ src/plugin/index-more/src/java/org/apache/nutch/indexer/more/MoreIndexingFilter.java
@@ -199,10 +199,13 @@ public class MoreIndexingFilter implements IndexingFilter {
 
     doc.add("type", scontentType);
 
-    String[] parts = getParts(scontentType);
+    // Check if we need to split the content type in sub parts
+    if (conf.getBoolean("moreIndexingFilter.indexMimeTypeParts", true)) {
+      String[] parts = getParts(contentType);
 
-    for (String part : parts) {
-      doc.add("type", part);
+      for(String part: parts) {
+        doc.add("type", part);
+      }
     }
 
     // leave this for future improvement
