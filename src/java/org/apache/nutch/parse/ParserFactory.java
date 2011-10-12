e71f40e4bf8c9d8eb417dfe6740f56a07cdab1f3
Lewis John McGibbney <lewismc@apache.org>
2011-10-12 18:18:48 +0000

    commit to address NUTCH-1097 and update to changes.txt
    git-svn-id: https://svn.apache.org/repos/asf/nutch/branches/nutchgora@1182504 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/nutch/parse/ParserFactory.java
+++ src/java/org/apache/nutch/parse/ParserFactory.java
@@ -356,13 +356,12 @@ public final class ParserFactory {
       // NotMappedParserException
 
       for (int i=0; i<extensions.length; i++) {
-        if (extensions[i].getAttribute("contentType") != null
-            && extensions[i].getAttribute("contentType").equals(
-                contentType)) {
-          extList.add(extensions[i]);
-        }
-        else if ("*".equals(extensions[i].getAttribute("contentType"))){
+      	if ("*".equals(extensions[i].getAttribute("contentType"))){
           extList.add(0, extensions[i]);
+        }
+      	else if (extensions[i].getAttribute("contentType") != null
+            && contentType.matches(escapeContentType(extensions[i].getAttribute("contentType")))) {
+          extList.add(extensions[i]);
         }
       }
 
@@ -391,10 +390,19 @@ public final class ParserFactory {
     return (extList.size() > 0) ? extList : null;
   }
 
-  private boolean match(Extension extension, String id, String type) {
-    return ((id.equals(extension.getId())) &&
-            (type.equals(extension.getAttribute("contentType")) || extension.getAttribute("contentType").equals("*") ||
-             type.equals(DEFAULT_PLUGIN)));
+  private String escapeContentType(String contentType) {
+  	// Escapes contentType in order to use as a regex 
+  	// (and keep backwards compatibility).
+  	// This enables to accept multiple types for a single parser. 
+  	return contentType.replace("+", "\\+").replace(".", "\\.");
+	}
+
+
+	private boolean match(Extension extension, String id, String type) {
+    return (id.equals(extension.getId())) &&
+            (extension.getAttribute("contentType").equals("*") ||
+             type.matches(escapeContentType(extension.getAttribute("contentType"))) ||
+             type.equals(DEFAULT_PLUGIN));
   }
 
   /** Get an extension from its id and supported content-type. */
