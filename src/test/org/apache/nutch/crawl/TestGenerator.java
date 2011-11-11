8b04a0c17a479c2a3c08e0d8899148bc1002804f
Markus Jelsma <markus@apache.org>
2011-11-11 15:01:05 +0000

    NUTCH-1155 Fixes failing test
    git-svn-id: https://svn.apache.org/repos/asf/nutch/trunk@1200912 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/nutch/crawl/TestGenerator.java
+++ src/test/org/apache/nutch/crawl/TestGenerator.java
@@ -144,7 +144,7 @@ public class TestGenerator extends TestCase {
     createCrawlDB(list);
 
     Configuration myConfiguration = new Configuration(conf);
-    myConfiguration.setInt(Generator.GENERATOR_MAX_COUNT, 1);
+    myConfiguration.setInt(Generator.GENERATOR_MAX_COUNT, 2);
     Path generatedSegment = generateFetchlist(Integer.MAX_VALUE,
         myConfiguration, false);
 
@@ -157,7 +157,7 @@ public class TestGenerator extends TestCase {
     assertEquals(1, fetchList.size());
 
     myConfiguration = new Configuration(conf);
-    myConfiguration.setInt(Generator.GENERATOR_MAX_COUNT, 2);
+    myConfiguration.setInt(Generator.GENERATOR_MAX_COUNT, 3);
     generatedSegment = generateFetchlist(Integer.MAX_VALUE, myConfiguration,
         false);
 
@@ -170,7 +170,7 @@ public class TestGenerator extends TestCase {
     assertEquals(2, fetchList.size());
 
     myConfiguration = new Configuration(conf);
-    myConfiguration.setInt(Generator.GENERATOR_MAX_COUNT, 3);
+    myConfiguration.setInt(Generator.GENERATOR_MAX_COUNT, 4);
     generatedSegment = generateFetchlist(Integer.MAX_VALUE, myConfiguration,
         false);
 
@@ -198,7 +198,7 @@ public class TestGenerator extends TestCase {
     createCrawlDB(list);
 
     Configuration myConfiguration = new Configuration(conf);
-    myConfiguration.setInt(Generator.GENERATOR_MAX_COUNT, 1);
+    myConfiguration.setInt(Generator.GENERATOR_MAX_COUNT, 2);
     myConfiguration.set(Generator.GENERATOR_COUNT_MODE, Generator.GENERATOR_COUNT_VALUE_DOMAIN);
 
     Path generatedSegment = generateFetchlist(Integer.MAX_VALUE,
@@ -213,7 +213,7 @@ public class TestGenerator extends TestCase {
     assertEquals(1, fetchList.size());
 
     myConfiguration = new Configuration(myConfiguration);
-    myConfiguration.setInt(Generator.GENERATOR_MAX_COUNT, 2);
+    myConfiguration.setInt(Generator.GENERATOR_MAX_COUNT, 3);
     generatedSegment = generateFetchlist(Integer.MAX_VALUE, myConfiguration, false);
 
     fetchlistPath = new Path(new Path(generatedSegment,
@@ -225,7 +225,7 @@ public class TestGenerator extends TestCase {
     assertEquals(2, fetchList.size());
 
     myConfiguration = new Configuration(myConfiguration);
-    myConfiguration.setInt(Generator.GENERATOR_MAX_COUNT, 3);
+    myConfiguration.setInt(Generator.GENERATOR_MAX_COUNT, 4);
     generatedSegment = generateFetchlist(Integer.MAX_VALUE, myConfiguration,
         false);
 
