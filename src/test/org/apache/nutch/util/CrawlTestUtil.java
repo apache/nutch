9538868c98beb351c64222f24d9755fd6a97d3b3
Ferdy Galema <ferdy@apache.org>
2012-05-04 08:04:21 +0000

    NUTCH-1350 remove unused dependancy because of access restriction
    git-svn-id: https://svn.apache.org/repos/asf/nutch/branches/nutchgora@1333803 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/nutch/util/CrawlTestUtil.java
+++ src/test/org/apache/nutch/util/CrawlTestUtil.java
@@ -39,9 +39,6 @@ import org.mortbay.jetty.Server;
 import org.mortbay.jetty.handler.DefaultHandler;
 import org.mortbay.jetty.handler.HandlerList;
 import org.mortbay.jetty.handler.ResourceHandler;
-import org.mortbay.jetty.servlet.Context;
-
-import com.sun.net.httpserver.HttpContext;
 
 public class CrawlTestUtil {
 
