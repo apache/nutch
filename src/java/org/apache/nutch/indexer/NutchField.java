789a174bd2b38201d99462ade317893e9c16afdd
Markus Jelsma <markus@apache.org>
2013-01-16 11:10:09 +0000

    Implement read/write in NutchField
    git-svn-id: https://svn.apache.org/repos/asf/nutch/trunk@1433900 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/nutch/indexer/NutchField.java
+++ src/java/org/apache/nutch/indexer/NutchField.java
@@ -22,9 +22,10 @@ import java.io.DataOutput;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
-import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.*;
 
 /**
  * This class represents a multi-valued field with a weight. Values are arbitrary
@@ -34,9 +35,7 @@ public class NutchField implements Writable {
   private float weight;
   private List<Object> values = new ArrayList<Object>();
   
-  public NutchField() {
-    
-  }
+  public NutchField() { }
   
   public NutchField(Object value) {
     this(value, 1.0f);
@@ -72,10 +71,64 @@ public class NutchField implements Writable {
     values.clear();
   }
 
+  @Override
+  public Object clone() throws CloneNotSupportedException {
+    NutchField result = (NutchField)super.clone();
+    result.weight = weight;
+    result.values = values;
+
+    return result;
+  }
+  
+  @Override
   public void readFields(DataInput in) throws IOException {
+    weight = in.readFloat();
+    int count = in.readInt();
+    values = new ArrayList<Object>();
+    for (int i = 0; i < count; i++) {
+      String type = Text.readString(in);
+      
+      if (type.equals("java.lang.String")) {
+        values.add(Text.readString(in));
+      } else if (type.equals("java.lang.Boolean")) {
+        values.add(in.readBoolean());
+      } else if (type.equals("java.lang.Integer")) {
+        values.add(in.readInt());
+      } else if (type.equals("java.lang.Float")) {
+        values.add(in.readFloat());
+      } else if (type.equals("java.util.Date")) {
+        values.add(new Date(in.readLong()));
+      }
+    }
   }
 
+  @Override
   public void write(DataOutput out) throws IOException {
+    out.writeFloat(weight);
+    out.writeInt(values.size());
+    for (Object value : values) {
+    
+      Text.writeString(out, value.getClass().getName());
+        
+      if (value instanceof Boolean) {
+        out.writeBoolean((Boolean)value);
+      } else if (value instanceof Integer) {
+        out.writeInt((Integer)value);
+      } else if (value instanceof Long) {
+        out.writeLong((Long)value);
+      } else if (value instanceof Float) {
+        out.writeFloat((Float)value);
+      } else if (value instanceof String) {
+        Text.writeString(out, (String)value);
+      } else if (value instanceof Date) {
+        Date date = (Date)value;
+        out.writeLong(date.getTime());
+      }
+    }
+  }
+  
+  public String toString() {
+    return values.toString();
   }
 
 }
