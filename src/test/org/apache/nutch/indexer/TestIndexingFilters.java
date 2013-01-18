f80299e4b3d5924aa940be9fa9d3299a63a36047
Lewis John McGibbney <lewismc@apache.org>
2013-01-18 20:38:55 +0000

    NUTCH-1453 Substantiate tests for IndexingFilters
    git-svn-id: https://svn.apache.org/repos/asf/nutch/branches/2.x@1435334 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/nutch/indexer/TestIndexingFilters.java
+++ src/test/org/apache/nutch/indexer/TestIndexingFilters.java
@@ -20,6 +20,7 @@ import junit.framework.TestCase;
 
 import org.apache.avro.util.Utf8;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.nutch.metadata.Metadata;
 import org.apache.nutch.storage.WebPage;
 import org.apache.nutch.util.NutchConfiguration;
 
@@ -31,18 +32,70 @@ public class TestIndexingFilters extends TestCase {
    */
   public void testNonExistingIndexingFilter() throws IndexingException {
     Configuration conf = NutchConfiguration.create();
+    conf.addResource("nutch-default.xml");
+    conf.addResource("crawl-tests.xml");
+
     String class1 = "NonExistingFilter";
     String class2 = "org.apache.nutch.indexer.basic.BasicIndexingFilter";
     conf.set(IndexingFilters.INDEXINGFILTER_ORDER, class1 + " " + class2);
 
     IndexingFilters filters = new IndexingFilters(conf);
-//    filters.filter(new NutchDocument(), new ParseImpl("text", new ParseData(
-//        new ParseStatus(), "title", new Outlink[0], new Metadata())), new Text(
-//        "http://www.example.com/"), new CrawlDatum(), new Inlinks());
     WebPage page = new WebPage();
     page.setText(new Utf8("text"));
     page.setTitle(new Utf8("title"));
     filters.filter(new NutchDocument(),"http://www.example.com/",page);
   }
+
+  /**
+   * Test behaviour when NutchDOcument is null
+   * @throws IndexingException
+   */
+  public void testNutchDocumentNullIndexingFilter() throws IndexingException{
+    Configuration conf = NutchConfiguration.create();
+    conf.addResource("nutch-default.xml");
+    conf.addResource("crawl-tests.xml");
+
+    IndexingFilters filters = new IndexingFilters(conf);
+    WebPage page = new WebPage();
+    page.setText(new Utf8("text"));
+    page.setTitle(new Utf8("title"));
+    NutchDocument doc = filters.filter(null,"http://www.example.com/",page);
+
+    assertNull(doc);
+  }
+
+    /**
+     * Test behaviour when reset the index filter order will not take effect
+     *
+     * @throws IndexingException
+     */
+    public void testFilterCacheIndexingFilter() throws IndexingException{
+        Configuration conf = NutchConfiguration.create();
+        conf.addResource("nutch-default.xml");
+        conf.addResource("crawl-tests.xml");
+
+        String class1 = "org.apache.nutch.indexer.basic.BasicIndexingFilter";
+        conf.set(IndexingFilters.INDEXINGFILTER_ORDER, class1);
+
+        IndexingFilters filters1 = new IndexingFilters(conf);
+        WebPage page = new WebPage();
+        page.setText(new Utf8("text"));
+        page.setTitle(new Utf8("title"));
+        NutchDocument fdoc1 = filters1.filter(new NutchDocument(),"http://www.example.com/",page);
+
+        // add another index filter
+        String class2 = "org.apache.nutch.indexer.metadata.MetadataIndexer";
+        // set content metadata
+        Metadata md = new Metadata();
+        md.add("example","data");
+        // set content metadata property defined in MetadataIndexer
+        conf.set("index.content.md","example");
+        // add MetadataIndxer filter
+        conf.set(IndexingFilters.INDEXINGFILTER_ORDER, class1 + " " + class2);
+        IndexingFilters filters2 = new IndexingFilters(conf);
+        NutchDocument fdoc2 = filters2.filter(new NutchDocument(),"http://www.example.com/",page);
+        assertEquals(fdoc1.getFieldNames().size(),fdoc2.getFieldNames().size());
+    }
+
 
 }
