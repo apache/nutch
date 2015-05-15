3f39b97af3e5b2cadc994f2584f8f9f316183f05
Sujen Shah <sujen1412@gmail.com>
2015-05-15 19:58:04 +0530

    Modified get function to return snapshot of current FetchNodeDb

--- src/java/org/apache/nutch/fetcher/FetchNodeDb.java
+++ src/java/org/apache/nutch/fetcher/FetchNodeDb.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -14,39 +14,40 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.nutch.fetcher;
 
-import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 
 public class FetchNodeDb {
 
-  private Map<String, FetchNode> fetchNodeDbMap;
+  private Map<Integer, FetchNode> fetchNodeDbMap;
+  private int index;
   private static FetchNodeDb fetchNodeDbInstance = null;
   
   public FetchNodeDb(){    
-    System.out.println("Calling FetchNode constructor");
-    fetchNodeDbMap = new ConcurrentHashMap<String, FetchNode>();
+//    System.out.println("Calling FetchNode constructor");
+    fetchNodeDbMap = new ConcurrentHashMap<Integer, FetchNode>();
+    index = 1;
   }
   
   public static FetchNodeDb getInstance(){
     
     if(fetchNodeDbInstance == null){
-      System.out.println("Creating FetchNode instance");
+//      System.out.println("Creating FetchNode instance");
       fetchNodeDbInstance = new FetchNodeDb();
     }
+//    System.out.println("FetchNodeDb Instance : " + fetchNodeDbInstance);
     return fetchNodeDbInstance;
   }
   
   public void put(String url, FetchNode fetchNode){
     System.out.println("FetchNodeDb : putting node - " + fetchNode.hashCode());
-    fetchNodeDbMap.put(url, fetchNode);    
+    fetchNodeDbMap.put(index++, fetchNode);    
   }  
-  public Iterator<?> getFetchNodeDb(){
-    return fetchNodeDbMap.entrySet().iterator();
+  public Map<Integer, FetchNode> getFetchNodeDb(){
+    return fetchNodeDbMap;
   }
 }
 
