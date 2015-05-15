45ab52f85f61d2e4cab9b6c8e24fa2191d44c7f2
Sujen Shah <sujen1412@gmail.com>
2015-05-15 20:39:17 +0530

    Removed debugging statements

--- src/java/org/apache/nutch/fetcher/FetchNode.java
+++ src/java/org/apache/nutch/fetcher/FetchNode.java
@@ -1,3 +1,19 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.nutch.fetcher;
 
 import org.apache.hadoop.io.Text;
@@ -14,49 +30,30 @@ public class FetchNode {
     return url;
   }
   public void setUrl(Text url) {
-    System.out.println(this.hashCode() + " Setting url to : " + url.toString());
     this.url = url;
   }
   public Outlink[] getOutlinks() {
     return outlinks;
   }
   public void setOutlinks(Outlink[] links) {
-    System.out.println(this.hashCode() + " Setting outlinks to : " + links.length);
     this.outlinks = links;
   }
   public int getStatus() {
     return status;
   }
   public void setStatus(int status) {
-    System.out.println(this.hashCode() + " Setting status to : " + status);
     this.status = status;
   }
   public String getTitle() {
     return title;
   }
   public void setTitle(String title) {
-    System.out.println(this.hashCode() + " Setting title to : " + title);
     this.title = title;
   }
   public long getFetchTime() {
     return fetchTime;
   }
   public void setFetchTime(long fetchTime) {
-    System.out.println(this.hashCode() + " Setting fetchTime to : " + fetchTime);
     this.fetchTime = fetchTime;
-  }
-  
-//  public String toString(){
-//    StringBuffer sb = new StringBuffer();
-//    sb.append("URL: "+this.getUrl().toString());
-//    sb.append("\n");
-//    sb.append("Title: "+this.getTitle());
-//    sb.append("No of Outlinks : " + this.getOutlinks().length);
-//    sb.append("Status: " + this.getStatus());
-//    
-//    return sb.toString();
-//  }
-  
-  
-  
+  }  
 }
