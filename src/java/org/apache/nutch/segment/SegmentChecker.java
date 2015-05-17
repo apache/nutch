529d32f9a330beff573570a8995e90711bd7395d
Chris Mattmann <mattmann@apache.org>
2015-05-17 23:57:44 +0000

    Re-apply NUTCH-1854 after mistakenly rolled back during NUTCH-1973.
    git-svn-id: https://svn.apache.org/repos/asf/nutch/trunk@1679911 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/nutch/segment/SegmentChecker.java
+++ src/java/org/apache/nutch/segment/SegmentChecker.java
@@ -113,6 +113,19 @@ public class SegmentChecker {
 
       return false;
     }
+
+  }
+
+  /**
+   * Check the segment to see if it is has been parsed before.
+   */
+  public static boolean isParsed(Path segment, FileSystem fs)
+      throws IOException {
+
+      if (fs.exists(new Path(segment, CrawlDatum.PARSE_DIR_NAME))){
+	return true;
+      }
+      return false;
   }
 
 }
