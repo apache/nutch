2b712c0d07b2d98fed4b3fb91542a78c7973d29b
Sujen Shah <sujen1412@gmail.com>
2015-06-14 16:48:03 -0700

    Overriding method calculate similarity

--- src/plugin/scoring-similarity/src/java/org/apache/nutch/scoring/similarity/CosineSimilarity.java
+++ src/plugin/scoring-similarity/src/java/org/apache/nutch/scoring/similarity/CosineSimilarity.java
@@ -5,13 +5,25 @@ import java.util.Map;
 public class CosineSimilarity {
 
   public double calculateCosineSimilarity(String goldStandard, String document2){
-    
+
     DocumentVector docVect1 = new DocumentVector(goldStandard);
     DocumentVector docVect2 = new DocumentVector(document2);
-    
+
     double doc1Dist = getEuclideanDist(docVect1);
     double doc2Dist = getEuclideanDist(docVect2);
-    
+
+    double dotProduct = getDotProduct(docVect1, docVect2);
+    if(doc1Dist*doc2Dist == 0){
+      return 0.0;
+    }
+    return dotProduct/(doc1Dist*doc2Dist);
+  }
+
+  public double calculateCosineSimilarity(DocumentVector docVect1, DocumentVector docVect2){
+
+    double doc1Dist = getEuclideanDist(docVect1);
+    double doc2Dist = getEuclideanDist(docVect2);
+
     double dotProduct = getDotProduct(docVect1, docVect2);
     if(doc1Dist*doc2Dist == 0){
       return 0.0;
@@ -25,13 +37,13 @@ public class CosineSimilarity {
     for(Map.Entry<String, Integer> pair : docVect1.getTermFreqVect().entrySet()){
       double doc1value = pair.getValue();
       double doc2value = 0;
-      
+
       if(doc2TermFreqVect.containsKey(pair.getKey()))
         doc2value = doc2TermFreqVect.get(pair.getKey());
-      
+
       dotProduct += doc1value*doc2value;
     }
-    
+
     return dotProduct;
   }
 
@@ -41,10 +53,5 @@ public class CosineSimilarity {
       sum += pair.getValue() * pair.getValue();
     }    
     return Math.sqrt(sum);
-  }
-  
-  public static void main(String[] args) {
-    CosineSimilarity cs = new CosineSimilarity();
-    System.out.println(cs.calculateCosineSimilarity("Classifieds listings of \nAll Categories in United States", "FREE Gun Classifieds - buy guns and sell guns online for FREE!"));
   }
 }
