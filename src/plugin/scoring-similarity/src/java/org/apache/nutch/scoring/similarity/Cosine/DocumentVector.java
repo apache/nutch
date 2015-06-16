15cf63dd8c4692a930d41a8657037fea1c8f7b26
Sujen Shah <sujen1412@gmail.com>
2015-06-16 16:00:29 -0700

    Caching stopwords to prevent it from loading everytime

--- src/plugin/scoring-similarity/src/java/org/apache/nutch/scoring/similarity/Cosine/DocumentVector.java
+++ src/plugin/scoring-similarity/src/java/org/apache/nutch/scoring/similarity/Cosine/DocumentVector.java
@@ -19,22 +19,34 @@ package org.apache.nutch.scoring.similarity.Cosine;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.util.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DocumentVector {
 
-  Map<String, Integer> termFreqVect;
+  private Map<String, Integer> termFreqVect;
+  private static Set<String> stopwordSet;
+  private final static Logger LOG = LoggerFactory
+      .getLogger(DocumentVector.class);
 
   public DocumentVector(String text, Configuration conf){
-    
+
     termFreqVect = new HashMap<String, Integer>();
+    if(stopwordSet == null){
+      stopwordSet = new HashSet<String>();
+      populateStopWordSet(conf);
+    }
     createDocVect(text);
     removeStopWords(conf);
   }
-  
+
   private void createDocVect(String text){
     if(text!=null){
       String[] tokens = text.replaceAll("[^a-zA-Z0-9 ]", " ").toLowerCase().split("\\s+");
@@ -48,24 +60,32 @@ public class DocumentVector {
       }
     }
   }
-  
+
   public Map<String, Integer> getTermFreqVect(){
     return termFreqVect;
   }
-  
+
   private void removeStopWords(Configuration conf){
+    for(String stopWord: stopwordSet){
+      stopWord = stopWord.trim();
+      if(termFreqVect.containsKey(stopWord)){
+        termFreqVect.remove(stopWord);
+      }
+    }
+  }
+
+  private void populateStopWordSet(Configuration conf){
     String stopWordFilePath = conf.get("scoring.similarity.stopword.file");
     Reader reader = conf.getConfResourceAsReader(stopWordFilePath);
     try {
+      LOG.info("Populating stopwords from {}", stopWordFilePath);
       String[] stopWordList1 = IOUtils.toString(reader).split("\n");
       for(String stopWord: stopWordList1){
         stopWord = stopWord.trim();
-        if(termFreqVect.containsKey(stopWord)){
-          termFreqVect.remove(stopWord);
-        }
+        stopwordSet.add(stopWord);
       }
     } catch (IOException e) {
-      // TODO Auto-generated catch block
+      LOG.error("Failed to populate stopwords : {}", StringUtils.stringifyException(e));
       e.printStackTrace();
     }
   }
