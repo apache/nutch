78007dd08f8c77cd75e5393f9d2e623be8ccbaf3
Sujen Shah <sujen1412@gmail.com>
2015-06-16 12:00:16 -0700

    Modefied SCoringFilter to make use of the similarity model interface class

--- src/plugin/scoring-similarity/src/java/org/apache/nutch/scoring/similarity/SimilarityScoringFilter.java
+++ src/plugin/scoring-similarity/src/java/org/apache/nutch/scoring/similarity/SimilarityScoringFilter.java
@@ -16,12 +16,9 @@
  */
 package org.apache.nutch.scoring.similarity;
 
-import java.io.IOException;
-import java.io.Reader;
 import java.util.Collection;
 import java.util.Map.Entry;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.Text;
 import org.apache.nutch.crawl.CrawlDatum;
@@ -31,16 +28,12 @@ import org.apache.nutch.parse.ParseData;
 import org.apache.nutch.protocol.Content;
 import org.apache.nutch.scoring.AbstractScoringFilter;
 import org.apache.nutch.scoring.ScoringFilterException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.nutch.scoring.similarity.Cosine.CosineSimilarityModel;
 
 public class SimilarityScoringFilter extends AbstractScoringFilter {
 
   private Configuration conf;
-  private String goldStandardDocPath;
-  private static DocumentVector goldStandardDocVect;
-  private final static Logger LOG = LoggerFactory
-      .getLogger(SimilarityScoringFilter.class);
+  private SimilarityModel similarityModel;
 
   @Override
   public Configuration getConf() {
@@ -50,47 +43,15 @@ public class SimilarityScoringFilter extends AbstractScoringFilter {
   @Override
   public void setConf(Configuration conf) {
     this.conf = conf;
-    goldStandardDocPath = conf.get("similarity.model.path");
-    Reader reader = conf.getConfResourceAsReader(goldStandardDocPath);
-    try {
-      String fileContent = IOUtils.toString(reader);
-      goldStandardDocVect = new DocumentVector(fileContent, conf);
-      LOG.info("Creating DocVector from path - {}",goldStandardDocPath);
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }    
+    similarityModel = new CosineSimilarityModel();
+    similarityModel.setConf(conf);
   }
 
   @Override
   public void passScoreAfterParsing(Text url, Content content, Parse parse)
       throws ScoringFilterException {
-    CosineSimilarity cs = new CosineSimilarity(conf);
-    
-    DocumentVector parseTextDocVect = new DocumentVector(parse.getText(), conf);
-    double parseTextSimilarity = cs.calculateCosineSimilarity(goldStandardDocVect, parseTextDocVect);
-  
-    DocumentVector parseMetaKeywordDocVect = new DocumentVector(parse.getData().getParseMeta().get("metatag.keyword"), conf);
-    double metaKeywordSimilarity = cs.calculateCosineSimilarity(goldStandardDocVect, parseMetaKeywordDocVect);
-    
-    DocumentVector parseMetaDescDocVect = new DocumentVector(parse.getData().getParseMeta().get("metatag.description"), conf);
-    double metaDescriptionSimilarity = cs.calculateCosineSimilarity(goldStandardDocVect, parseMetaDescDocVect);
-    
-    LOG.info("Calculating similarity between gold-standard and {}",url);
-    int count = 0;
-    if(parseTextSimilarity!=0)
-      count++;
-    if(metaDescriptionSimilarity!=0)
-      count++;
-    if(metaKeywordSimilarity!=0)
-      count++;
-    if(count==0)
-      count++;
 
-    double score =  (parseTextSimilarity+metaDescriptionSimilarity + metaKeywordSimilarity)/count;
-    LOG.info("Setting score of {} to {}",url, score);
-    LOG.info("Score break down TextSimilarity : {}, metaKeywordSimilarity : {}, metaDescriptionSimilarity : {}",
-        parseTextSimilarity, metaKeywordSimilarity, metaDescriptionSimilarity);
+    float score = similarityModel.setURLScoreAfterParsing(url, content, parse);
     parse.getData().getContentMeta()
     .set(Nutch.SCORE_KEY, score+"");
   }
@@ -99,11 +60,7 @@ public class SimilarityScoringFilter extends AbstractScoringFilter {
   public CrawlDatum distributeScoreToOutlinks(Text fromUrl,
       ParseData parseData, Collection<Entry<Text, CrawlDatum>> targets,
       CrawlDatum adjust, int allCount) throws ScoringFilterException {
-    float score = Float.parseFloat(parseData.getContentMeta().get(Nutch.SCORE_KEY));
-    for (Entry<Text, CrawlDatum> target : targets) {
-      target.getValue().setScore((float)score);
-      LOG.info("Setting score of {} to {}",target.getKey(), score);
-    }
+    similarityModel.distributeScoreToOutlinks(fromUrl, parseData, targets, adjust, allCount);
     return adjust;
   }
 }
