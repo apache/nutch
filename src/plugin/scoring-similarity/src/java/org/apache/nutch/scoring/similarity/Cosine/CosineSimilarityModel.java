5a439ae1591377450e9cf4a1a7b24efeeebc9d6d
Sujen Shah <sujen1412@gmail.com>
2015-06-16 12:00:50 -0700

    Implementing the new interface

--- src/plugin/scoring-similarity/src/java/org/apache/nutch/scoring/similarity/Cosine/CosineSimilarityModel.java
+++ src/plugin/scoring-similarity/src/java/org/apache/nutch/scoring/similarity/Cosine/CosineSimilarityModel.java
@@ -14,20 +14,35 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.nutch.scoring.similarity;
+package org.apache.nutch.scoring.similarity.Cosine;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Collection;
 import java.util.Map;
+import java.util.Map.Entry;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.Text;
+import org.apache.nutch.crawl.CrawlDatum;
+import org.apache.nutch.metadata.Nutch;
+import org.apache.nutch.parse.Parse;
+import org.apache.nutch.parse.ParseData;
+import org.apache.nutch.protocol.Content;
+import org.apache.nutch.scoring.similarity.SimilarityModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class CosineSimilarity {
+public class CosineSimilarityModel implements SimilarityModel{
 
   private Configuration conf;
+  private String goldStandardDocPath;
+  private static DocumentVector goldStandardDocVect;
+  private final static Logger LOG = LoggerFactory
+      .getLogger(CosineSimilarityModel.class);
   
-  public CosineSimilarity(Configuration conf){
-    this.conf = conf;
-  }
-  public double calculateCosineSimilarity(String goldStandard, String document2){
+  private double calculateCosineSimilarity(String goldStandard, String document2){
 
     DocumentVector docVect1 = new DocumentVector(goldStandard, conf);
     DocumentVector docVect2 = new DocumentVector(document2, conf);
@@ -42,7 +57,7 @@ public class CosineSimilarity {
     return dotProduct/(doc1Dist*doc2Dist);
   }
 
-  public double calculateCosineSimilarity(DocumentVector docVect1, DocumentVector docVect2){
+  private double calculateCosineSimilarity(DocumentVector docVect1, DocumentVector docVect2){
 
     double doc1Dist = getEuclideanDist(docVect1);
     double doc2Dist = getEuclideanDist(docVect2);
@@ -76,5 +91,62 @@ public class CosineSimilarity {
       sum += pair.getValue() * pair.getValue();
     }    
     return Math.sqrt(sum);
+  }
+  
+  @Override
+  public void setConf(Configuration conf) {
+    this.conf = conf;
+    goldStandardDocPath = conf.get("similarity.model.path");
+    Reader reader = conf.getConfResourceAsReader(goldStandardDocPath);
+    try {
+      String fileContent = IOUtils.toString(reader);
+      if(goldStandardDocVect == null) {
+        goldStandardDocVect = new DocumentVector(fileContent, conf);
+        LOG.info("Creating DocVector from path - {}",goldStandardDocPath);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }    
+  }
+  @Override
+  public float setURLScoreAfterParsing(Text url, Content content, Parse parse) {
+
+    DocumentVector parseTextDocVect = new DocumentVector(parse.getText(), conf);
+    double parseTextSimilarity = calculateCosineSimilarity(goldStandardDocVect, parseTextDocVect);
+  
+    DocumentVector parseMetaKeywordDocVect = new DocumentVector(parse.getData().getParseMeta().get("metatag.keyword"), conf);
+    double metaKeywordSimilarity = calculateCosineSimilarity(goldStandardDocVect, parseMetaKeywordDocVect);
+    
+    DocumentVector parseMetaDescDocVect = new DocumentVector(parse.getData().getParseMeta().get("metatag.description"), conf);
+    double metaDescriptionSimilarity = calculateCosineSimilarity(goldStandardDocVect, parseMetaDescDocVect);
+    
+    LOG.info("Calculating similarity between gold-standard and {}",url);
+    int count = 0;
+    if(parseTextSimilarity!=0)
+      count++;
+    if(metaDescriptionSimilarity!=0)
+      count++;
+    if(metaKeywordSimilarity!=0)
+      count++;
+    if(count==0)
+      count++;
+
+    float score =  (float) ((parseTextSimilarity+metaDescriptionSimilarity + metaKeywordSimilarity)/count);
+    LOG.info("Setting score of {} to {}",url, score);
+    LOG.info("Score break down TextSimilarity : {}, metaKeywordSimilarity : {}, metaDescriptionSimilarity : {}",
+        parseTextSimilarity, metaKeywordSimilarity, metaDescriptionSimilarity);
+    return score;
+  }
+  
+  @Override
+  public CrawlDatum distributeScoreToOutlinks(Text fromUrl,
+      ParseData parseData, Collection<Entry<Text, CrawlDatum>> targets,
+      CrawlDatum adjust, int allCount) {
+    float score = Float.parseFloat(parseData.getContentMeta().get(Nutch.SCORE_KEY));
+    for (Entry<Text, CrawlDatum> target : targets) {
+      target.getValue().setScore((float)score);
+      LOG.info("Setting score of {} to {}",target.getKey(), score);
+    }
+    return adjust;
   }
 }
