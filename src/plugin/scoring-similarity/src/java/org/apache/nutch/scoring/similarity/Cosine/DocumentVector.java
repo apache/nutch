f75beefe0b95df8680d07ae37a6365ea5483fdbb
Sujen Shah <sujen1412@gmail.com>
2015-06-16 12:01:32 -0700

    Moved to package Cosine

--- src/plugin/scoring-similarity/src/java/org/apache/nutch/scoring/similarity/Cosine/DocumentVector.java
+++ src/plugin/scoring-similarity/src/java/org/apache/nutch/scoring/similarity/Cosine/DocumentVector.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.nutch.scoring.similarity;
+package org.apache.nutch.scoring.similarity.Cosine;
 
 import java.io.IOException;
 import java.io.Reader;
