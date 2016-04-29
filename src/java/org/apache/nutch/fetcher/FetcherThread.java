0e03daf1139a1a8465d6e2a6b54490e2dfc2a9ef
Sebastian Nagel <snagel@apache.org>
2016-04-29 18:33:04 +0200

    Inconsistent log level

--- src/java/org/apache/nutch/fetcher/FetcherThread.java
+++ src/java/org/apache/nutch/fetcher/FetcherThread.java
@@ -286,7 +286,7 @@ public class FetcherThread extends Thread {
                     .getFetchItemQueue(fit.queueID);
                 fiq.crawlDelay = rules.getCrawlDelay();
                 if (LOG.isDebugEnabled()) {
-                  LOG.info("Crawl delay for queue: " + fit.queueID
+                  LOG.debug("Crawl delay for queue: " + fit.queueID
                       + " is set to " + fiq.crawlDelay
                       + " as per robots.txt. url: " + fit.url);
                 }
