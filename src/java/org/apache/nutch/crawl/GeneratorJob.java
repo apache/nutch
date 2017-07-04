7981df9a1a018e9d24d2440ec7e0917fb1b47789
Lewis John McGibbney <lewis.mcgibbney@gmail.com>
2017-07-04 12:52:08 -0700

    NUTCH-2395 Cannot run job worker! - error while running multiple crawling jobs in parallel

--- src/java/org/apache/nutch/crawl/GeneratorJob.java
+++ src/java/org/apache/nutch/crawl/GeneratorJob.java
@@ -153,6 +153,11 @@ public class GeneratorJob extends NutchTool implements Tool {
     public SelectorEntryComparator() {
       super(SelectorEntry.class, true);
     }
+
+    @Override
+    synchronized public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
+      return super.compare(b1, s1, l1, b2, s2, l2);
+    } 
   }
 
   static {
