93fb5395478e982e45e8bebbf69435db1a8ce5e7
Sebastian Nagel <snagel@apache.org>
2017-08-15 22:16:26 +0200

    NUTCH-2378 ChildFirst plugin classloader (contributed by Jurian Broertjes)

--- src/java/org/apache/nutch/plugin/PluginClassLoader.java
+++ src/java/org/apache/nutch/plugin/PluginClassLoader.java
@@ -20,20 +20,27 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.Arrays;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
+
 /**
- * The <code>PluginClassLoader</code> contains only classes of the runtime
- * libraries setuped in the plugin manifest file and exported libraries of
- * plugins that are required pluguin. Libraries can be exported or not. Not
- * exported libraries are only used in the plugin own
+ * The <code>PluginClassLoader</code> is a child-first classloader that only
+ * contains classes of the runtime libraries setuped in the plugin manifest file
+ * and exported libraries of plugins that are required plugins. Libraries can be
+ * exported or not. Not exported libraries are only used in the plugin own
  * <code>PluginClassLoader</code>. Exported libraries are available for
  * <code>PluginClassLoader</code> of plugins that depends on these plugins.
  * 
- * @author joa23
  */
 public class PluginClassLoader extends URLClassLoader {
 
   private URL[] urls;
   private ClassLoader parent;
+  private ClassLoader system = getSystemClassLoader();
 
   /**
    * Construtor
@@ -48,6 +55,126 @@ public class PluginClassLoader extends URLClassLoader {
 
     this.urls = urls;
     this.parent = parent;
+  }
+
+  @Override
+  protected synchronized Class<?> loadClass(String name, boolean resolve)
+      throws ClassNotFoundException {
+
+    // First, check if the class has already been loaded
+    Class<?> c = findLoadedClass(name);
+
+    if (c == null) {
+      try {
+        // checking local
+        c = findClass(name);
+      } catch (ClassNotFoundException | SecurityException e) {
+        c = loadClassFromParent(name, resolve);
+      }
+    }
+
+    if (resolve) {
+      resolveClass(c);
+    }
+
+    return c;
+  }
+
+  private Class<?> loadClassFromParent(String name, boolean resolve)
+      throws ClassNotFoundException {
+    // checking parent
+    // This call to loadClass may eventually call findClass
+    // again, in case the parent doesn't find anything.
+    Class<?> c;
+    try {
+      c = super.loadClass(name, resolve);
+    } catch (ClassNotFoundException e) {
+      c = loadClassFromSystem(name);
+    } catch (SecurityException e) {
+      c = loadClassFromSystem(name);
+    }
+    return c;
+  }
+
+  private Class<?> loadClassFromSystem(String name)
+      throws ClassNotFoundException {
+    Class<?> c = null;
+    if (system != null) {
+      // checking system: jvm classes, endorsed, cmd classpath,
+      c = system.loadClass(name);
+    }
+    return c;
+  }
+
+  @Override
+  public URL getResource(String name) {
+    URL url = findResource(name);
+    if (url == null)
+      url = super.getResource(name);
+
+    if (url == null && system != null)
+      url = system.getResource(name);
+
+    return url;
+  }
+
+  @Override
+  public Enumeration<URL> getResources(String name) throws IOException {
+    /**
+     * Similar to super, but local resources are enumerated before parent
+     * resources
+     */
+    Enumeration<URL> systemUrls = null;
+    if (system != null) {
+      systemUrls = system.getResources(name);
+    }
+
+    Enumeration<URL> localUrls = findResources(name);
+    Enumeration<URL> parentUrls = null;
+    if (getParent() != null) {
+      parentUrls = getParent().getResources(name);
+    }
+
+    final List<URL> urls = new ArrayList<URL>();
+    if (localUrls != null) {
+      while (localUrls.hasMoreElements()) {
+        URL local = localUrls.nextElement();
+        urls.add(local);
+      }
+    }
+
+    if (systemUrls != null) {
+      while (systemUrls.hasMoreElements()) {
+        urls.add(systemUrls.nextElement());
+      }
+    }
+
+    if (parentUrls != null) {
+      while (parentUrls.hasMoreElements()) {
+        urls.add(parentUrls.nextElement());
+      }
+    }
+
+    return new Enumeration<URL>() {
+      Iterator<URL> iter = urls.iterator();
+
+      public boolean hasMoreElements() {
+        return iter.hasNext();
+      }
+
+      public URL nextElement() {
+        return iter.next();
+      }
+    };
+  }
+
+  public InputStream getResourceAsStream(String name) {
+    URL url = getResource(name);
+    try {
+      return url != null ? url.openStream() : null;
+    } catch (IOException e) {
+    }
+    return null;
   }
 
   @Override
