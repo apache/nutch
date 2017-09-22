6e527687bc13bb88ef022c22cb94e06019297fd5
Hiran Chaudhuri <hiran.chaudhuri@mail.de>
2017-09-22 16:36:57 +0200

    This is an example plugin for the "foo" protocol. It is not a full implementation, take it more as a showcase for the plugin system modifications that allow new protocols to be introduced with a minimum installation effort.
    The skeleton implementation we have so far allows foo://... urls to be
    injected into crawldb.

---
+++ src/plugin/protocol-foo/src/java/my/foo/Foo.java
@@ -0,0 +1,4 @@
+package my.foo;
+
+public class Foo {
+}
---
+++ src/plugin/protocol-foo/src/java/my/foo/Handler.java
@@ -0,0 +1,12 @@
+package my.foo;
+
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLStreamHandler;
+
+public class Handler extends URLStreamHandler {
+
+	protected URLConnection openConnection(URL u) {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+}
