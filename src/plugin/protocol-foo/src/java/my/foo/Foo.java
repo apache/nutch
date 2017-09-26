c1b9b60c8a6fe3be94766d988995e04eb067a502
Hiran Chaudhuri <hiran.chaudhuri@mail.de>
2017-09-26 22:52:44 +0200

    The protocol-foo plugin will respond to query with minimum empty data. Empty content seems to make nutch fail later.

--- src/plugin/protocol-foo/src/java/my/foo/Foo.java
+++ src/plugin/protocol-foo/src/java/my/foo/Foo.java
@@ -1,4 +1,49 @@
 package my.foo;
 
-public class Foo {
+import java.util.List;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.Text;
+import org.apache.nutch.crawl.CrawlDatum;
+import org.apache.nutch.plugin.URLStreamHandlerFactory;
+import org.apache.nutch.protocol.Content;
+import org.apache.nutch.protocol.Protocol;
+import org.apache.nutch.protocol.ProtocolOutput;
+import org.apache.nutch.protocol.ProtocolStatus;
+import org.apache.nutch.protocol.RobotRulesParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import crawlercommons.robots.BaseRobotRules;
+
+public class Foo implements Protocol {
+  protected static final Logger LOG = LoggerFactory
+      .getLogger(Foo.class);
+  
+  private Configuration conf;
+
+  @Override
+  public Configuration getConf() {
+    LOG.debug("getConf()");
+    return conf;
+  }
+
+  @Override
+  public void setConf(Configuration conf) {
+    LOG.debug("setConf(...)");
+    this.conf = conf;
+  }
+
+  @Override
+  public ProtocolOutput getProtocolOutput(Text url, CrawlDatum datum) {
+    LOG.debug("getProtocolOutput("+url+", "+datum+")");
+    return new ProtocolOutput(new Content(), ProtocolStatus.STATUS_SUCCESS);
+  }
+
+  @Override
+  public BaseRobotRules getRobotRules(Text url, CrawlDatum datum,
+      List<Content> robotsTxtContent) {
+    LOG.debug("getRobotRules("+url+", "+datum+", "+robotsTxtContent+")");
+    return RobotRulesParser.EMPTY_RULES;
+  }
 }
