d02c279a3cb40d0eeccca08c9d2982e611a4949c
Hiran Chaudhuri <hiran.chaudhuri@mail.de>
2017-10-10 09:08:05 +0200

    Commented why we make use of WeakReference.

--- src/java/org/apache/nutch/plugin/URLStreamHandlerFactory.java
+++ src/java/org/apache/nutch/plugin/URLStreamHandlerFactory.java
@@ -9,6 +9,15 @@ import org.mortbay.log.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * This URLStreamHandlerFactory knows about all the plugins
+ * in use and thus can create the correct URLStreamHandler
+ * even if it comes from a plugin classpath.
+ * As the JVM allows only one instance of URLStreamHandlerFactory
+ * to be registered, this class implements a singleton pattern.
+ * @author Hiran Chaudhuri
+ *
+ */
 public class URLStreamHandlerFactory
     implements java.net.URLStreamHandlerFactory {
   
@@ -18,7 +27,14 @@ public class URLStreamHandlerFactory
   /** The singleton instance. */
   private static URLStreamHandlerFactory instance;
   
-  /** Here we register all PluginRepositories. */
+  /** Here we register all PluginRepositories.
+   * In this class we do not know why several instances of PluginRepository
+   * are kept, nor do we know how long they will be used. To prevent
+   * a memory leak, this class must not keep references to PluginRepository
+   * but use WeakReference which allows PluginRepository to still be
+   * garbage collected. The prize is we need to clean the list for
+   * outdated references which is done in the {@link removeInvalidRefs} method.  
+   */
   private ArrayList<WeakReference<PluginRepository>> prs;
   
   static {
