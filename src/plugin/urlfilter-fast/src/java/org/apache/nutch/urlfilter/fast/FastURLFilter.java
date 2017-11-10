9a98edf33806f00d9159a882093572c67d637d5d
Sebastian Nagel <sebastian@commoncrawl.org>
2017-11-10 14:54:29 +0100

    Speed-up urlfilter-fast: - fast and singleton implementation for frequently used   "deny all" rules ("DenyPath .*") Sync with version on branch commoncrawl/nutch/cc

--- src/plugin/urlfilter-fast/src/java/org/apache/nutch/urlfilter/fast/FastURLFilter.java
+++ src/plugin/urlfilter-fast/src/java/org/apache/nutch/urlfilter/fast/FastURLFilter.java
@@ -30,7 +30,22 @@ import java.net.URI;
 import java.util.regex.Pattern;
 
 /**
- * Filters URLs based on a file of regular expressions and host/domains using the
+ * Filters URLs based on a file of regular expressions using host/domains matching
+ * first. Host-based rules are evaluated before domain-based rules, but otherwise should
+ * be checked in order. The default policy is to accept a URL if no matches are found.
+ *
+ * Rule Format:
+ *
+ * Host www.imdb.com
+ *   DenyPath /keyword/(?:.*?/){2,}  # permutation of keywords separated by /
+ *   DenyPath /some/other/path
+ *
+ * Domain gravatar.com
+ *   DenyPath .*     # Deny everything from *.gravatar.com and gravatar.com
+ *
+ * Domain myspace.com
+ *   DenyPathQuery /resource/.*?action=add
+ *
  * {@link java.util.regex Java Regex implementation}.
  */
 public class FastURLFilter implements URLFilter {
@@ -39,7 +54,6 @@ public class FastURLFilter implements URLFilter {
   public static final String URLFILTER_FAST_FILE = "urlfilter.fast.file";
   private Multimap<String, Rule> hostRules = LinkedHashMultimap.create();
   private Multimap<String, Rule> domainRules = LinkedHashMultimap.create();
-  int cnt = 0;
 
   public FastURLFilter() {}
 
@@ -74,6 +88,13 @@ public class FastURLFilter implements URLFilter {
           }
         }
       }
+
+      // Finally match the Domain '.' which allows us to build simple global rules
+      for (Rule rule : domainRules.get(".")) {
+        if (rule.match(uri)) {
+          return null;
+        }
+      }
     } catch (Exception e) {
       return null;
     }
@@ -82,17 +103,6 @@ public class FastURLFilter implements URLFilter {
     return url;
   }
 
-  /* Format:
-     Host www.imdb.com
-         DenyPath /keyword/(?:.*?/){2,}  # permutation of keywords separated by /
-         DenyPath /some/other/path
-
-     Domain gravatar.com
-        DenyPath .*     # Deny everything from *.gravatar.com and gravatar.com
-
-     Domain myspace.com
-        DenyPathQuery /resource/.*?action=add
-   */
   public void reloadRules() {
     domainRules.clear();
     hostRules.clear();
@@ -100,12 +110,15 @@ public class FastURLFilter implements URLFilter {
     String fileRules = conf.get(URLFILTER_FAST_FILE);
     BufferedReader reader = new BufferedReader(conf.getConfResourceAsReader(fileRules));
 
+
     String current = null;
     boolean host = false;
+    int lineno = 0;
 
     String line;
     try {
       while((line = reader.readLine()) != null) {
+        lineno++;
         line = line.trim();
 
         if (line.indexOf("#") != -1) {
@@ -127,30 +140,36 @@ public class FastURLFilter implements URLFilter {
             continue;
           }
 
-          if (line.startsWith("DenyPath ")) {
-            String rule = line.split("\\s")[1];
-
-            if (host) {
-              hostRules.put(current, new DenyPathRule(rule));
+          Rule rule = null;
+          try {
+            if (line.startsWith("DenyPath ")) {
+              if (line.equals("DenyPath .*")) {
+                rule = DenyPathEntirelyRule.getInstance();
+              } else {
+                rule = new DenyPathRule(line.split("\\s")[1]);
+              }
+            } else if (line.startsWith("DenyPathQuery ")) {
+              rule = new DenyPathQueryRule(line.split("\\s")[1]);
             } else {
-              domainRules.put(current, new DenyPathRule(rule));
+              continue;
             }
-          } else if (line.startsWith("DenyPathQuery ")) {
-            String rule = line.split("\\s")[1];
+          } catch (IndexOutOfBoundsException e) {
+            LOG.warn("Problem reading rule on line " + lineno + ": " + line);
+          }
 
-            if (host) {
-              hostRules.put(current, new DenyPathQueryRule(rule));
-            } else {
-              domainRules.put(current, new DenyPathQueryRule(rule));
-            }
+          if (host) {
+            if (LOG.isTraceEnabled()) { LOG.trace("Adding host rule [" + current + "] [" + rule.toString() + "]"); }
+            hostRules.put(current,rule);
+          } else {
+            if (LOG.isTraceEnabled()) { LOG.trace("Adding domain rule [" + current + "] [" + rule.toString() + "]"); }
+            domainRules.put(current, rule);
           }
         }
       }
     } catch (IOException e) {
-      LOG.warn("Caught exception while reading rules file");
+      LOG.warn("Caught exception while reading rules file at line " + lineno);
     }
   }
-
 
   public static class Rule {
     protected Pattern pattern;
@@ -182,6 +201,23 @@ public class FastURLFilter implements URLFilter {
       }
 
       return pattern.matcher(haystack).find();
+    }
+  }
+
+  /** Rule for &quot;DenyPath .*&quot; */
+  public static class DenyPathEntirelyRule extends Rule {
+
+    private static Rule instance = new DenyPathEntirelyRule(".*");
+
+    private DenyPathEntirelyRule(String pattern) {
+    }
+
+    public static Rule getInstance() {
+      return instance;
+    }
+
+    public boolean match(URI uri) {
+      return true;
     }
   }
 
