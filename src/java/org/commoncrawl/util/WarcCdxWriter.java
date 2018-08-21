4fc4f065089452b2cb3cd8043d62874614b0c317
Sebastian Nagel <sebastian@commoncrawl.org>
2018-08-21 12:01:44 +0200

    Common Crawl WARC writer/exporter Fix cdx output of revisit records (HTTP status 304 notmodified): - set "mime" to "warc/revisit" (as done by PyWB) - no "mime-detected" - add payload "digest" (required by columnar Parquet index)

--- src/java/org/commoncrawl/util/WarcCdxWriter.java
+++ src/java/org/commoncrawl/util/WarcCdxWriter.java
@@ -31,6 +31,7 @@ import java.util.TimeZone;
 import org.apache.commons.io.output.CountingOutputStream;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.util.StringUtils;
+import org.apache.nutch.metadata.Metadata;
 import org.apache.nutch.protocol.Content;
 import org.archive.url.WaybackURLKeyMaker;
 import org.slf4j.Logger;
@@ -100,7 +101,7 @@ public class WarcCdxWriter extends WarcWriter {
         relatedId, warcProfile, refersToDate, payloadDigest, blockDigest, block,
         content);
     long length = (countingOut.getByteCount() - offset);
-    writeCdxLine(targetUri, date, offset, length, null, content);
+    writeCdxLine(targetUri, date, offset, length, payloadDigest, content, true);
     return recordId;
   }
 
@@ -114,14 +115,16 @@ public class WarcCdxWriter extends WarcWriter {
         warcinfoId, relatedId, payloadDigest, blockDigest, truncated, block,
         content);
     long length = (countingOut.getByteCount() - offset);
-    writeCdxLine(targetUri, date, offset, length, payloadDigest, content);
+    writeCdxLine(targetUri, date, offset, length, payloadDigest, content, false);
     return recordId;
   }
 
   public void writeCdxLine(final URI targetUri, final Date date, long offset,
-      long length, String payloadDigest, Content content) throws IOException {
+      long length, String payloadDigest, Content content, boolean revisit)
+      throws IOException {
     String url = targetUri.toString();
     String surt = url;
+    Metadata meta = content.getMetadata();
     try {
       surt = surtKeyMaker.makeKey(url);
     } catch (URISyntaxException e) {
@@ -140,9 +143,13 @@ public class WarcCdxWriter extends WarcWriter {
     cdxOut.write(' ');
     Map<String, String> data = new LinkedHashMap<String, String>();
     data.put("url", url);
-    data.put("mime", cleanMimeType(content.getMetadata().get("Content-Type")));
-    data.put("mime-detected", content.getContentType());
-    data.put("status", content.getMetadata().get("HTTP-Status-Code"));
+    if (revisit) {
+      data.put("mime", "warc/revisit");
+    } else {
+      data.put("mime", cleanMimeType(meta.get("Content-Type")));
+      data.put("mime-detected", content.getContentType());
+    }
+    data.put("status", meta.get("HTTP-Status-Code"));
     if (payloadDigest != null) {
       data.put("digest", payloadDigest);
     }
