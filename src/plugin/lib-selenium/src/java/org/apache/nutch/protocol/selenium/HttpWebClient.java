6d71d0d9663a855a05430c69f3fbcb78caa7fc32
root <s.batururimi@gmail.com>
2018-11-16 12:37:24 +0000

    remote web driver with headless option added to code

--- src/plugin/lib-selenium/src/java/org/apache/nutch/protocol/selenium/HttpWebClient.java
+++ src/plugin/lib-selenium/src/java/org/apache/nutch/protocol/selenium/HttpWebClient.java
@@ -24,26 +24,38 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
+import java.util.Random;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
-import org.openqa.selenium.By;
+
+//import org.openqa.selenium.By;
+import org.openqa.selenm.Capabilities;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
 import org.openqa.selenium.io.TemporaryFilesystem;
+
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+
 import org.openqa.selenium.safari.SafariDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+
+//import org.openqa.selenium.phantomjs.PhantomJSDriver;
+//import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -62,98 +74,98 @@ public class HttpWebClient {
 //      FirefoxProfile profile = new FirefoxProfile();
 //      profile.setPreference("permissions.default.stylesheet", 2);
 //      profile.setPreference("permissions.default.image", 2);
-//      profile.setPreference("dom.ipc.plugins.enabled.libflashplayer.so", "false");
-//      profile.setPreference(FirefoxProfile.ALLOWED_HOSTS_PREFERENCE, "localhost");
-//      WebDriver driver = new FirefoxDriver(profile);
-//      return driver;          
-//    };
-//  };
-
-  public static WebDriver getDriverForPage(String url, Configuration conf) {
-      WebDriver driver = null;
-      DesiredCapabilities capabilities = null;
-      long pageLoadWait = conf.getLong("page.load.delay", 3);
-
-      try {
-        String driverType  = conf.get("selenium.driver", "firefox");
-        switch (driverType) {
-          case "firefox":
-          	String allowedHost = conf.get("selenium.firefox.allowed.hosts", "localhost");
-          	long firefoxBinaryTimeout = conf.getLong("selenium.firefox.binary.timeout", 45);
-          	boolean enableFlashPlayer = conf.getBoolean("selenium.firefox.enable.flash", false);
-          	int loadImage = conf.getInt("selenium.firefox.load.image", 1);
-          	int loadStylesheet = conf.getInt("selenium.firefox.load.stylesheet", 1);
-    		    FirefoxProfile profile = new FirefoxProfile();
-    		    FirefoxBinary binary = new FirefoxBinary();
-    		    profile.setPreference(FirefoxProfile.ALLOWED_HOSTS_PREFERENCE, allowedHost);
-    		    profile.setPreference("dom.ipc.plugins.enabled.libflashplayer.so", enableFlashPlayer);
-    		    profile.setPreference("permissions.default.stylesheet", loadStylesheet);
-  	      	profile.setPreference("permissions.default.image", loadImage);
-    		    binary.setTimeout(TimeUnit.SECONDS.toMillis(firefoxBinaryTimeout));
-            driver = new FirefoxDriver(binary, profile);
-            break;
-          case "chrome":
-            driver = new ChromeDriver();
-            break;
-          case "safari":
-            driver = new SafariDriver();
-            break;
-          case "opera":
-            driver = new OperaDriver();
-            break;
-          case "phantomjs":
-            driver = new PhantomJSDriver();
-            break;
-          case "remote":
-            String seleniumHubHost = conf.get("selenium.hub.host", "localhost");
-            int seleniumHubPort = Integer.parseInt(conf.get("selenium.hub.port", "4444"));
-            String seleniumHubPath = conf.get("selenium.hub.path", "/wd/hub");
-            String seleniumHubProtocol = conf.get("selenium.hub.protocol", "http");
-            String seleniumGridDriver = conf.get("selenium.grid.driver","firefox");
-            String seleniumGridBinary = conf.get("selenium.grid.binary");
-
-            switch (seleniumGridDriver){
-              case "firefox":
-                capabilities = DesiredCapabilities.firefox();
-                capabilities.setBrowserName("firefox");
-                capabilities.setJavascriptEnabled(true);
-                capabilities.setCapability("firefox_binary",seleniumGridBinary);
-                System.setProperty("webdriver.reap_profile", "false");
-                driver = new RemoteWebDriver(new URL(seleniumHubProtocol, seleniumHubHost, seleniumHubPort, seleniumHubPath), capabilities);
-                break;
-              case "phantomjs":
-                capabilities = DesiredCapabilities.phantomjs();
-                capabilities.setBrowserName("phantomjs");
-                capabilities.setJavascriptEnabled(true);
-                capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,seleniumGridBinary);
-                driver = new RemoteWebDriver(new URL(seleniumHubProtocol, seleniumHubHost, seleniumHubPort, seleniumHubPath), capabilities);
-                break;
-              default:
-                LOG.error("The Selenium Grid WebDriver choice {} is not available... defaulting to FirefoxDriver().", driverType);
-                driver = new RemoteWebDriver(new URL(seleniumHubProtocol, seleniumHubHost, seleniumHubPort, seleniumHubPath), DesiredCapabilities.firefox());
-                break;
-            }
-            break;
-          default:
-            LOG.error("The Selenium WebDriver choice {} is not available... defaulting to FirefoxDriver().", driverType);
-            driver = new FirefoxDriver();
-            break;
-        }
-        LOG.debug("Selenium {} WebDriver selected.", driverType);
-  
-        driver.manage().timeouts().pageLoadTimeout(pageLoadWait, TimeUnit.SECONDS);
-        driver.get(url);
-      } catch (Exception e) {
-			  if(e instanceof TimeoutException) {
-          LOG.debug("Selenium WebDriver: Timeout Exception: Capturing whatever loaded so far...");
-          return driver;
-			  }
-			  cleanUpDriver(driver);
-		    throw new RuntimeException(e);
-	    } 
-
-      return driver;
-  }
+////      profile.setPreference("dom.ipc.plugins.enabled.libflashplayer.so", "false");
+////      profile.setPreference(FirefoxProfile.ALLOWED_HOSTS_PREFERENCE, "localhost");
+////      WebDriver driver = new FirefoxDriver(profile);
+////      return driver;          
+////    };
+////  };
+//
+//  public static WebDriver getDriverForPage(String url, Configuration conf) {
+//      WebDriver driver = null;
+//      DesiredCapabilities capabilities = null;
+//      long pageLoadWait = conf.getLong("page.load.delay", 3);
+//
+//      try {
+//        String driverType  = conf.get("selenium.driver", "firefox");
+//        switch (driverType) {
+//          case "firefox":
+//          	String allowedHost = conf.get("selenium.firefox.allowed.hosts", "localhost");
+//          	long firefoxBinaryTimeout = conf.getLong("selenium.firefox.binary.timeout", 45);
+//          	boolean enableFlashPlayer = conf.getBoolean("selenium.firefox.enable.flash", false);
+//          	int loadImage = conf.getInt("selenium.firefox.load.image", 1);
+//          	int loadStylesheet = conf.getInt("selenium.firefox.load.stylesheet", 1);
+//    		    FirefoxProfile profile = new FirefoxProfile();
+//    		    FirefoxBinary binary = new FirefoxBinary();
+//    		    profile.setPreference(FirefoxProfile.ALLOWED_HOSTS_PREFERENCE, allowedHost);
+//    		    profile.setPreference("dom.ipc.plugins.enabled.libflashplayer.so", enableFlashPlayer);
+//    		    profile.setPreference("permissions.default.stylesheet", loadStylesheet);
+//  	      	profile.setPreference("permissions.default.image", loadImage);
+//    		    binary.setTimeout(TimeUnit.SECONDS.toMillis(firefoxBinaryTimeout));
+//            driver = new FirefoxDriver(binary, profile);
+//            break;
+//          case "chrome":
+//            driver = new ChromeDriver();
+//            break;
+//          case "safari":
+//            driver = new SafariDriver();
+//            break;
+//          case "opera":
+//            driver = new OperaDriver();
+//            break;
+//          case "phantomjs":
+//            driver = new PhantomJSDriver();
+//            break;
+//          case "remote":
+//            String seleniumHubHost = conf.get("selenium.hub.host", "localhost");
+//            int seleniumHubPort = Integer.parseInt(conf.get("selenium.hub.port", "4444"));
+//            String seleniumHubPath = conf.get("selenium.hub.path", "/wd/hub");
+//            String seleniumHubProtocol = conf.get("selenium.hub.protocol", "http");
+//            String seleniumGridDriver = conf.get("selenium.grid.driver","firefox");
+//            String seleniumGridBinary = conf.get("selenium.grid.binary");
+//
+//            switch (seleniumGridDriver){
+//              case "firefox":
+//                capabilities = DesiredCapabilities.firefox();
+//                capabilities.setBrowserName("firefox");
+//                capabilities.setJavascriptEnabled(true);
+//                capabilities.setCapability("firefox_binary",seleniumGridBinary);
+//                System.setProperty("webdriver.reap_profile", "false");
+//                driver = new RemoteWebDriver(new URL(seleniumHubProtocol, seleniumHubHost, seleniumHubPort, seleniumHubPath), capabilities);
+//                break;
+//              case "phantomjs":
+//                capabilities = DesiredCapabilities.phantomjs();
+//                capabilities.setBrowserName("phantomjs");
+//                capabilities.setJavascriptEnabled(true);
+//                capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,seleniumGridBinary);
+//                driver = new RemoteWebDriver(new URL(seleniumHubProtocol, seleniumHubHost, seleniumHubPort, seleniumHubPath), capabilities);
+//                break;
+//              default:
+//                LOG.error("The Selenium Grid WebDriver choice {} is not available... defaulting to FirefoxDriver().", driverType);
+//                driver = new RemoteWebDriver(new URL(seleniumHubProtocol, seleniumHubHost, seleniumHubPort, seleniumHubPath), DesiredCapabilities.firefox());
+//                break;
+//            }
+//            break;
+//          default:
+//            LOG.error("The Selenium WebDriver choice {} is not available... defaulting to FirefoxDriver().", driverType);
+//            driver = new FirefoxDriver();
+//            break;
+//        }
+//        LOG.debug("Selenium {} WebDriver selected.", driverType);
+//  
+//        driver.manage().timeouts().pageLoadTimeout(pageLoadWait, TimeUnit.SECONDS);
+//        driver.get(url);
+//      } catch (Exception e) {
+//			  if(e instanceof TimeoutException) {
+//          LOG.debug("Selenium WebDriver: Timeout Exception: Capturing whatever loaded so far...");
+//          return driver;
+//			  }
+//			  cleanUpDriver(driver);
+//		    throw new RuntimeException(e);
+//	    } 
+//
+//      return driver;
+//  }
 
 //  public static String getHTMLContent(WebDriver driver, Configuration conf) {
 //      if (conf.getBoolean("take.screenshot", false)) {
@@ -162,6 +174,93 @@ public class HttpWebClient {
 //
 //      return driver.findElement(By.tagName("body")).getAttribute("innerHTML");
 //  }
+
+  public static WebDriver getDriverForPage(String url, Configuration conf) {
+    WebDriver driver = null;
+    try{
+	    String driverType  = conf.get("selenium.driver", "firefox");
+	    switch (driverType) {
+		    case "remote":
+			    String seleniumHubHost = conf.get("selenium.hub.host", "localhost");
+			    int seleniumHubPort = Integer.parseInt(conf.get("selenium.hub.port", "4444"));
+			    String seleniumHubPath = conf.get("selenium.hub.path", "/wd/hub");
+			    String seleniumHubProtocol = conf.get("selenium.hub.protocol", "http");
+			    URL seleniumHubUrl = new URL(seleniumHubProtocol, seleniumHubHost, seleniumHubPort, seleniumHubPath); 
+
+			    String seleniumGridDriver = conf.get("selenium.grid.driver","firefox");
+			    boolean enableHeadlessMode = conf.getBoolean("selenium.enable.headless", false);
+
+			    switch (seleniumGridDriver){
+				    case "firefox":
+					    driver = createFirefoxWebDriver(seleniumHubUrl, enableHeadlessMode);
+					    break;
+				    case "chrome":
+					    driver = createChromeWebDriver(seleniumHubUrl, enableHeadlessMode);
+					    break;
+				    case "random":
+					    driver = createRandomWebDriver(seleniumHubUrl, enableHeadlessMode);
+					    break;
+				    default:
+					    LOG.error("The Selenium Grid WebDriver choice {} is not available... defaulting to FirefoxDriver().", driverType);
+					    driver = createDefaultWebDriver(seleniumHubUrl, enableHeadlessMode); 
+					    break;
+			    }
+
+	    }
+    }catch (Exception e){
+	    if(e instanceof TimeoutException){
+		    LOG.debug("Selenium WebDriver: Timeout Exception: Capturing whatever loaded so far...");
+		    return driver;
+	    }
+	    cleanUpDriver(driver);
+	    throw new RuntimeException(e);
+    }
+
+  }
+
+  public static Capabilities createFirefoxWebDriver(URL seleniumHubUrl, boolean enableHeadlessMode){
+    FirefoxOptions firefoxOptions = new FirefoxOptions();
+    if(enableHeadlessMode){
+	    firefoxOptions.setHeadless(true);
+    }
+    RemoteWebDriver driver = createRemoteWebDriver(seleniumHubUrl, firefoxOptions);
+    return driver;
+  }
+  
+  public static Capabilities createChromeWebDriver(URL seleniumHubUrl, boolean enableHeadlessMode){
+    ChromeOptions = new ChromeOptions();
+    if(enableHeadlessMode){
+	    chromeOptions.setHeadless(true);
+    }
+    RemoteWebDriver driver = createRemoteWebDriver(seleniumHubUrl, firefoxOptions);
+    return driver;
+  }
+
+  public static Capabilities createRandomWebDriver(URL seleniumHubUrl, boolean enableHeadlessMode){
+    // we consider a possibility of generating only 2 types of browsers: Firefox and Chrome only
+    Random r = new Random();
+    int min = 0;
+    // we have actually hardcoded the maximum number of types of web driver that can be created
+    // but this must be later moved to the configuration file in order to be able
+    // to randomly choose between much more types(ex: Edge, Opera, Safari)
+    int max = 1; // for 3 types, change to 2 and update the if-clause
+    int num =  r.nextInt((max - min) + 1) + min;
+    if(num == 0){
+	return createFirefoxWebDriver(seleniumHubUrl, enableHeadlessMode);
+    }
+    
+    return createChromeWebDriver(seleniumHubUrl, enableHeadlessMode); 
+  }
+
+  public static Capabilities createDefaultWebDriver(URL seleniumHubUrl, boolean enableHeadlessMode){
+    return createFirefoxWebDriver(enableHeadlessMode);	  
+  }
+
+  public static RemoteWebDriver createRemoteWebDriver(URL url, Capabilities capabilities){
+    RemoteWebDriver driver = new RemoteWebDriver(url, capabilities);
+    return driver; 
+  }
+
 
   public static void cleanUpDriver(WebDriver driver) {
     if (driver != null) {
