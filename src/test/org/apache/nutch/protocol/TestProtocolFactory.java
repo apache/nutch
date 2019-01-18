64a6f7cc533f41e53cf6b637979080235b2147fa
Markus Jelsma <markus@apache.org>
2019-01-18 13:27:16 +0100

    NUTCH-2678 Allow for per-host configurable protocol plugin

--- src/test/org/apache/nutch/protocol/TestProtocolFactory.java
+++ src/test/org/apache/nutch/protocol/TestProtocolFactory.java
@@ -59,12 +59,6 @@ public class TestProtocolFactory {
       Assert.fail("Must not throw any other exception");
     }
 
-    // cache key
-    Object protocol = ObjectCache.get(conf).getObject(
-        Protocol.X_POINT_ID + "http");
-    Assert.assertNotNull(protocol);
-    Assert.assertEquals(httpProtocol, protocol);
-
     // test same object instance
     try {
       Assert.assertTrue(httpProtocol == factory.getProtocol("http://somehost"));
