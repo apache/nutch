bb7c1d4aef3a17ddcfb12f48d2e022e67d5128eb
Lewis John McGibbney <lewis.mcgibbney@gmail.com>
2022-06-15 19:15:05 -0700

    NUTCH-2940 Develop Gradle Core Build for Apache Nutch

--- src/plugin/sample-plugin/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-plugins {
-    id("application")
-}
-
-application {
-    mainClass.set("java.org.apache.nutch.example")
-}
--- src/plugin/sample-plugin/src/java/org/apache/nutch/example/Hello.java
+++
@@ -1,7 +0,0 @@
-package java.org.apache.nutch.example;
-
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
-}
